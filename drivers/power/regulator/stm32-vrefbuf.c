RaitoBezarius/uboot-nwa50ax

commit 2836d5ff3ed2b5ab89ed9f21f9afbab8710e5d9d

power: regulator: Add support for stm32-vrefbuf
Add regulator driver for STM32 voltage reference buffer which can be
used as voltage reference for ADCs, DACs and external components through
dedicated VREF+ pin.

Signed-off-by: Fabrice Gasnier <fabrice.gasnier@st.com>
Signed-off-by: Patrice Chotard <patrice.chotard@st.com>

drivers/power/regulator/stm32-vrefbuf.c
@@ -1,0 +1,155 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2018, STMicroelectronics - All Rights Reserved
+ * Author: Fabrice Gasnier <fabrice.gasnier@st.com>
+ *
+ * Originally based on the Linux kernel v4.16 drivers/regulator/stm32-vrefbuf.c
+ */
+
+#include <common.h>
+#include <clk.h>
+#include <dm.h>
+#include <asm/io.h>
+#include <linux/iopoll.h>
+#include <linux/kernel.h>
+#include <power/regulator.h>
+
+/* STM32 VREFBUF registers */
+#define STM32_VREFBUF_CSR		0x00
+
+/* STM32 VREFBUF CSR bitfields */
+#define STM32_VRS			GENMASK(6, 4)
+#define STM32_VRS_SHIFT			4
+#define STM32_VRR			BIT(3)
+#define STM32_HIZ			BIT(1)
+#define STM32_ENVR			BIT(0)
+
+struct stm32_vrefbuf {
+	void __iomem *base;
+	struct clk clk;
+	struct udevice *vdda_supply;
+};
+
+static const unsigned int stm32_vrefbuf_voltages[] = {
+	/* Matches resp. VRS = 000b, 001b, 010b, 011b */
+	2500000, 2048000, 1800000, 1500000,
+};
+
+static int stm32_vrefbuf_set_enable(struct udevice *dev, bool enable)
+{
+	struct stm32_vrefbuf *priv = dev_get_priv(dev);
+	u32 val;
+	int ret;
+
+	clrsetbits_le32(priv->base + STM32_VREFBUF_CSR, STM32_HIZ | STM32_ENVR,
+			enable ? STM32_ENVR : STM32_HIZ);
+	if (!enable)
+		return 0;
+
+	/*
+	 * Vrefbuf startup time depends on external capacitor: wait here for
+	 * VRR to be set. That means output has reached expected value.
+	 * ~650us sleep should be enough for caps up to 1.5uF. Use 10ms as
+	 * arbitrary timeout.
+	 */
+	ret = readl_poll_timeout(priv->base + STM32_VREFBUF_CSR, val,
+				 val & STM32_VRR, 10000);
+	if (ret < 0) {
+		dev_err(dev, "stm32 vrefbuf timed out: %d\n", ret);
+		clrsetbits_le32(priv->base + STM32_VREFBUF_CSR, STM32_ENVR,
+				STM32_HIZ);
+		return ret;
+	}
+
+	return 0;
+}
+
+static int stm32_vrefbuf_get_enable(struct udevice *dev)
+{
+	struct stm32_vrefbuf *priv = dev_get_priv(dev);
+
+	return readl(priv->base + STM32_VREFBUF_CSR) & STM32_ENVR;
+}
+
+static int stm32_vrefbuf_set_value(struct udevice *dev, int uV)
+{
+	struct stm32_vrefbuf *priv = dev_get_priv(dev);
+	unsigned int i;
+
+	for (i = 0; i < ARRAY_SIZE(stm32_vrefbuf_voltages); i++) {
+		if (uV == stm32_vrefbuf_voltages[i]) {
+			clrsetbits_le32(priv->base + STM32_VREFBUF_CSR,
+					STM32_VRS, i << STM32_VRS_SHIFT);
+			return 0;
+		}
+	}
+
+	return -EINVAL;
+}
+
+static int stm32_vrefbuf_get_value(struct udevice *dev)
+{
+	struct stm32_vrefbuf *priv = dev_get_priv(dev);
+	u32 val;
+
+	val = readl(priv->base + STM32_VREFBUF_CSR) & STM32_VRS;
+	val >>= STM32_VRS_SHIFT;
+
+	return stm32_vrefbuf_voltages[val];
+}
+
+static const struct dm_regulator_ops stm32_vrefbuf_ops = {
+	.get_value  = stm32_vrefbuf_get_value,
+	.set_value  = stm32_vrefbuf_set_value,
+	.get_enable = stm32_vrefbuf_get_enable,
+	.set_enable = stm32_vrefbuf_set_enable,
+};
+
+static int stm32_vrefbuf_probe(struct udevice *dev)
+{
+	struct stm32_vrefbuf *priv = dev_get_priv(dev);
+	int ret;
+
+	priv->base = dev_read_addr_ptr(dev);
+
+	ret = clk_get_by_index(dev, 0, &priv->clk);
+	if (ret) {
+		dev_err(dev, "Can't get clock: %d\n", ret);
+		return ret;
+	}
+
+	ret = clk_enable(&priv->clk);
+	if (ret) {
+		dev_err(dev, "Can't enable clock: %d\n", ret);
+		return ret;
+	}
+
+	ret = device_get_supply_regulator(dev, "vdda-supply",
+					  &priv->vdda_supply);
+	if (ret) {
+		dev_dbg(dev, "No vdda-supply: %d\n", ret);
+		return 0;
+	}
+
+	ret = regulator_set_enable(priv->vdda_supply, true);
+	if (ret) {
+		dev_err(dev, "Can't enable vdda-supply: %d\n", ret);
+		clk_disable(&priv->clk);
+	}
+
+	return ret;
+}
+
+static const struct udevice_id stm32_vrefbuf_ids[] = {
+	{ .compatible = "st,stm32-vrefbuf" },
+	{ }
+};
+
+U_BOOT_DRIVER(stm32_vrefbuf) = {
+	.name  = "stm32-vrefbuf",
+	.id = UCLASS_REGULATOR,
+	.of_match = stm32_vrefbuf_ids,
+	.probe = stm32_vrefbuf_probe,
+	.ops = &stm32_vrefbuf_ops,
+	.priv_auto_alloc_size = sizeof(struct stm32_vrefbuf),
+};

drivers/power/regulator/Kconfig
@@ -197,6 +197,15 @@ config DM_REGULATOR_LP87565
 	be configured in multi phase modes. The driver implements
 	get/set api for value and enable.
 
+config DM_REGULATOR_STM32_VREFBUF
+	bool "Enable driver for STMicroelectronics STM32 VREFBUF"
+	depends on DM_REGULATOR && (STM32H7 || ARCH_STM32MP)
+	help
+	This driver supports STMicroelectronics STM32 VREFBUF (voltage
+	reference buffer) which can be used as voltage reference for
+	internal ADCs, DACs and also for external components through
+	dedicated Vref+ pin.
+
 config DM_REGULATOR_TPS65910
 	bool "Enable driver for TPS65910 PMIC regulators"
 	depends on DM_PMIC_TPS65910

drivers/power/regulator/Makefile
@@ -22,5 +22,6 @@ obj-$(CONFIG_$(SPL_)DM_REGULATOR_PALMAS) += palmas_regulator.o
 obj-$(CONFIG_$(SPL_)DM_REGULATOR_PBIAS) += pbias_regulator.o
 obj-$(CONFIG_$(SPL_)DM_REGULATOR_LP873X) += lp873x_regulator.o
 obj-$(CONFIG_$(SPL_)DM_REGULATOR_LP87565) += lp87565_regulator.o
+obj-$(CONFIG_$(SPL_)DM_REGULATOR_STM32_VREFBUF) += stm32-vrefbuf.o
 obj-$(CONFIG_DM_REGULATOR_TPS65910) += tps65910_regulator.o
 obj-$(CONFIG_$(SPL_)DM_REGULATOR_STPMU1) += stpmu1.o

ThingLabsOSS/superbird-uboot

commit c1476e9c5525227cfbd43417035772a3b2fc9005

power: regulator: stm32: vrefbuf: fix a possible overshoot when re-enabling
There maybe an overshoot:
- when disabling, then re-enabling vrefbuf too quickly
- or upon platform reset as external capacitor maybe slow
  discharging (VREFBUF is HiZ at reset by default).
VREFBUF is used by ADC/DAC on some boards. An overshoot on the reference
voltage make the conversions inaccurate for a short period of time. So:
- Don't put the VREFBUF in HiZ when disabling, to force an active
  discharge.
- Enforce a 1ms OFF/ON delay, also upon reset

Penalty is a 1ms delay is applied (even for a cold boot), when enabling
VREFBUF.

Fixes: 2836d5ff3ed2 ("power: regulator: Add support for stm32-vrefbuf")

Signed-off-by: Fabrice Gasnier <fabrice.gasnier@st.com>
Signed-off-by: Patrick Delaunay <patrick.delaunay@st.com>
Reviewed-by: Patrice Chotard <patrice.chotard@st.com>
Reviewed-by: Jaehoon Chung <jh80.chung@samsung.com>

drivers/power/regulator/stm32-vrefbuf.c
@@ -43,8 +43,20 @@ static int stm32_vrefbuf_set_enable(struct udevice *dev, bool enable)
 	u32 val;
 	int ret;
 
-	clrsetbits_le32(priv->base + STM32_VREFBUF_CSR, STM32_HIZ | STM32_ENVR,
-			enable ? STM32_ENVR : STM32_HIZ);
+	if (enable && !(readl(priv->base + STM32_VREFBUF_CSR) & STM32_ENVR)) {
+		/*
+		 * There maybe an overshoot:
+		 * - when disabling, then re-enabling vrefbuf too quickly
+		 * - or upon platform reset as external capacitor maybe slow
+		 *   discharging (VREFBUF is HiZ at reset by default).
+		 * So force active discharge (HiZ=0) for 1ms before enabling.
+		 */
+		clrbits_le32(priv->base + STM32_VREFBUF_CSR, STM32_HIZ);
+		udelay(1000);
+	}
+
+	clrsetbits_le32(priv->base + STM32_VREFBUF_CSR, STM32_ENVR,
+			enable ? STM32_ENVR : 0);
 	if (!enable)
 		return 0;
 