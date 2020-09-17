ThingLabsOSS/superbird-uboot

commit d46f02cb2893940fd2c99162ef10bb7321a9164c

toradex: common: show boot logo
Add function for showing boot logo, embed into u-boot blob.

Signed-off-by: Igor Opaniuk <igor.opaniuk@toradex.com>

board/toradex/common/tdx-common.c
@@ -9,6 +9,13 @@
 #include <init.h>
 #include <linux/libfdt.h>
 
+#ifdef CONFIG_DM_VIDEO
+#include <bmp_logo.h>
+#include <dm.h>
+#include <splash.h>
+#include <video.h>
+#endif
+
 #include "tdx-cfg-block.h"
 #include <asm/setup.h>
 #include "tdx-common.h"
@@ -196,3 +203,22 @@ int ft_common_board_setup(void *blob, struct bd_info *bd)
 }
 
 #endif /* CONFIG_TDX_CFG_BLOCK */
+
+#if defined(CONFIG_DM_VIDEO)
+int show_boot_logo(void)
+{
+	struct udevice *dev;
+	int ret;
+	int xpos, ypos;
+
+	splash_get_pos(&xpos, &ypos);
+
+	ret = uclass_get_device(UCLASS_VIDEO, 0, &dev);
+	if (ret)
+		return ret;
+
+	ret = video_bmp_display(dev, (ulong)bmp_logo_bitmap, xpos, ypos, true);
+
+	return ret;
+}
+#endif /* CONFIG_DM_VIDEO */

board/toradex/common/tdx-common.h
@@ -11,4 +11,8 @@
 
 int ft_common_board_setup(void *blob, struct bd_info *bd);
 
+#if defined(CONFIG_DM_VIDEO)
+int show_boot_logo(void);
+#endif
+
 #endif /* _TDX_COMMON_H */

commit 1f1c19715be41ad5cbe592a2dcd940e7b4b4635b

toradex: drop legacy show_boot_logo function and use splashscreen
Drop show_boot_logo legacy function, as splashscreen functionality can
be used instead.

Fixes: d46f02cb28("toradex: common: show boot logo")
Signed-off-by: Igor Opaniuk <igor.opaniuk@toradex.com>

board/toradex/common/tdx-common.c
@@ -203,22 +203,3 @@ int ft_common_board_setup(void *blob, struct bd_info *bd)
 }
 
 #endif /* CONFIG_TDX_CFG_BLOCK */
-
-#if defined(CONFIG_DM_VIDEO)
-int show_boot_logo(void)
-{
-	struct udevice *dev;
-	int ret;
-	int xpos, ypos;
-
-	splash_get_pos(&xpos, &ypos);
-
-	ret = uclass_get_device(UCLASS_VIDEO, 0, &dev);
-	if (ret)
-		return ret;
-
-	ret = video_bmp_display(dev, (ulong)bmp_logo_bitmap, xpos, ypos, true);
-
-	return ret;
-}
-#endif /* CONFIG_DM_VIDEO */