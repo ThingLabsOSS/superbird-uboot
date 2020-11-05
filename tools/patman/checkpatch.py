ThingLabsOSS/superbird-uboot

commit caf3b3b1afafec6e16f5fa9428c881ad8041479d

patman: Handle checkpatch output with notes and code
If checkpatch is configured to output code we should ignore it. Similarly,
notes should be ignored.

Update the logic to handle these situations.

Signed-off-by: Simon Glass <sjg@chromium.org>

tools/patman/checkpatch.py
@@ -85,7 +85,8 @@ def CheckPatch(fname, verbose=False):
     re_warning = re.compile(emacs_prefix + 'WARNING:(?:[A-Z_]+:)? (.*)')
     re_check = re.compile('CHECK: (.*)')
     re_file = re.compile('#\d+: FILE: ([^:]*):(\d+):')
-
+    re_note = re.compile('NOTE: (.*)')
+    indent = ' ' * 6
     for line in result.stdout.splitlines():
         if verbose:
             print(line)
@@ -95,6 +96,14 @@ def CheckPatch(fname, verbose=False):
             if item:
                 result.problems.append(item)
                 item = {}
+            continue
+        if re_note.match(line):
+            continue
+        # Skip lines which quote code
+        if line.startswith(indent):
+            continue
+        # Skip code quotes and #<n>
+        if line.startswith('+') or line.startswith('#'):
             continue
         match = re_stats_full.match(line)
         if not match:

commit e5618614e435d055d22131aecf37ef028495cb17

patman: Don't ignore lines starting with hash
These lines can indicate a continuation of an error and should not be
ignored. Fix this.

Fixes: caf3b3b1afa ("patman: Handle checkpatch output with notes and code")

Signed-off-by: Simon Glass <sjg@chromium.org>

tools/patman/checkpatch.py
@@ -95,6 +95,7 @@ def CheckPatch(fname, verbose=False, show_types=False):
     re_check = re.compile('CHECK:%s (.*)' % type_name)
     re_file = re.compile('#\d+: FILE: ([^:]*):(\d+):')
     re_note = re.compile('NOTE: (.*)')
+    re_new_file = re.compile('new file mode .*')
     indent = ' ' * 6
     for line in result.stdout.splitlines():
         if verbose:
@@ -111,8 +112,10 @@ def CheckPatch(fname, verbose=False, show_types=False):
         # Skip lines which quote code
         if line.startswith(indent):
             continue
-        # Skip code quotes and #<n>
-        if line.startswith('+') or line.startswith('#'):
+        # Skip code quotes
+        if line.startswith('+'):
+            continue
+        if re_new_file.match(line):
             continue
         match = re_stats_full.match(line)
         if not match: