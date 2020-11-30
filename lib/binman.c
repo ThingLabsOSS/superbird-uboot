ThingLabsOSS/superbird-uboot

commit 91b19688a5b5db0bb04869ea65ce362d709be932

binman: Call helper function binman_set_rom_offset() to fill offset
There is prepared function for filing rom_offset. That's why use it instead
of copying content of it.

Signed-off-by: Michal Simek <michal.simek@xilinx.com>
Reviewed-by: Simon Glass <sjg@chromium.org>
Reviewed-by: Simon Glass <sjg@chromium.org>

lib/binman.c
@@ -103,7 +103,7 @@ int binman_init(void)
 			return log_msg_ret("first image", -ENOENT);
 		binman->image = node;
 	}
-	binman->rom_offset = ROM_OFFSET_NONE;
-
+	binman_set_rom_offset(ROM_OFFSET_NONE);
+\
 	return 0;
 }

commit d60fce1a6d239886b77e8f1bac7233ec0fa13b15

binman: Remove additional backslash
The origin patch didn't have this change and it was caused by manual
resolution where additional backslash was added.

Fixes: 91b19688a5b5 ("binman: Call helper function binman_set_rom_offset() to fill offset")
Signed-off-by: Michal Simek <michal.simek@xilinx.com>
Reviewed-by: Simon Glass <sjg@chromium.org>

lib/binman.c
@@ -104,6 +104,6 @@ int binman_init(void)
 		binman->image = node;
 	}
 	binman_set_rom_offset(ROM_OFFSET_NONE);
-\
+
 	return 0;
 }