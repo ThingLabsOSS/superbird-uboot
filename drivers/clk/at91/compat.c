ThingLabsOSS/superbird-uboot

commit 1ba1a9c7e08e25fe9758f26fc23ddc03d0bddbec

dm: Remove uses of device_bind_offset()
This function is not needed since the standard device_bind() can be used
instead.

Signed-off-by: Simon Glass <sjg@chromium.org>

drivers/clk/at91/compat.c
@@ -62,34 +62,30 @@ static int at91_pmc_core_probe(struct udevice *dev)
  */
 int at91_clk_sub_device_bind(struct udevice *dev, const char *drv_name)
 {
-	const void *fdt = gd->fdt_blob;
-	int offset = dev_of_offset(dev);
+	ofnode parent = dev_ofnode(dev);
+	ofnode node;
 	bool pre_reloc_only = !(gd->flags & GD_FLG_RELOC);
 	const char *name;
 	int ret;
 
-	for (offset = fdt_first_subnode(fdt, offset);
-	     offset > 0;
-	     offset = fdt_next_subnode(fdt, offset)) {
-		if (pre_reloc_only &&
-		    !ofnode_pre_reloc(offset_to_ofnode(offset)))
+	ofnode_for_each_subnode(node, parent) {
+		if (pre_reloc_only && !ofnode_pre_reloc(node))
 			continue;
 		/*
 		 * If this node has "compatible" property, this is not
 		 * a clock sub-node, but a normal device. skip.
 		 */
-		fdt_get_property(fdt, offset, "compatible", &ret);
-		if (ret >= 0)
+		if (ofnode_read_prop(node, "compatible", NULL))
 			continue;
 
 		if (ret != -FDT_ERR_NOTFOUND)
 			return ret;
 
-		name = fdt_get_name(fdt, offset, NULL);
+		name = ofnode_get_name(node);
 		if (!name)
 			return -EINVAL;
-		ret = device_bind_driver_to_node(dev, drv_name, name,
-					offset_to_ofnode(offset), NULL);
+		ret = device_bind_driver_to_node(dev, drv_name, name, node,
+						 NULL);
 		if (ret)
 			return ret;
 	}

arch/x86/cpu/apollolake/spl.c
@@ -86,7 +86,7 @@ static int apl_flash_probe(struct udevice *dev)
 /*
  * Manually set the parent of the SPI flash to SPI, since dtoc doesn't. We also
  * need to allocate the parent_platdata since by the time this function is
- * called device_bind_offset() has already gone past that step.
+ * called device_bind() has already gone past that step.
  */
 static int apl_flash_bind(struct udevice *dev)
 {

drivers/clk/clk.c
@@ -36,7 +36,7 @@ int clk_register(struct clk *clk, const char *drv_name,
 		return -ENOENT;
 	}
 
-	ret = device_bind_offset(parent, drv, name, NULL, -1, &clk->dev);
+	ret = device_bind(parent, drv, name, NULL, ofnode_null(), &clk->dev);
 	if (ret) {
 		printf("%s: CLK: %s driver bind error [%d]!\n", __func__, name,
 		       ret);

drivers/gpio/mt7621_gpio.c
@@ -157,8 +157,8 @@ static int gpio_mediatek_bind(struct udevice *parent)
 		plat->gpio_count = MTK_BANK_WIDTH;
 		plat->bank = bank;
 
-		ret = device_bind_offset(parent, parent->driver,
-					 plat->bank_name, plat, -1, &dev);
+		ret = device_bind(parent, parent->driver, plat->bank_name, plat,
+				  ofnode_null(), &dev);
 		if (ret)
 			return ret;
 

drivers/gpio/s5p_gpio.c
@@ -332,8 +332,8 @@ static int gpio_exynos_bind(struct udevice *parent)
 			return -ENOMEM;
 
 		plat->bank_name = fdt_get_name(blob, node, NULL);
-		ret = device_bind_offset(parent, parent->driver,
-					 plat->bank_name, plat, -1, &dev);
+		ret = device_bind(parent, parent->driver, plat->bank_name, plat,
+				  ofnode_null(), &dev);
 		if (ret)
 			return ret;
 

drivers/gpio/sunxi_gpio.c
@@ -305,8 +305,8 @@ static int gpio_sunxi_bind(struct udevice *parent)
 		plat->bank_name = gpio_bank_name(soc_data->start + bank);
 		plat->gpio_count = SUNXI_GPIOS_PER_BANK;
 
-		ret = device_bind_offset(parent, parent->driver,
-					 plat->bank_name, plat, -1, &dev);
+		ret = device_bind(parent, parent->driver, plat->bank_name, plat,
+				  ofnode_null(), &dev);
 		if (ret)
 			return ret;
 		dev_set_of_offset(dev, dev_of_offset(parent));

drivers/gpio/tegra186_gpio.c
@@ -190,8 +190,8 @@ static int tegra186_gpio_bind(struct udevice *parent)
 		plat->name = ctlr_data->ports[port].name;
 		plat->regs = &(regs[ctlr_data->ports[port].offset / 4]);
 
-		ret = device_bind_offset(parent, parent->driver, plat->name,
-					 plat, -1, &dev);
+		ret = device_bind(parent, parent->driver, plat->name, plat,
+				  ofnode_null(), &dev);
 		if (ret)
 			return ret;
 		dev_set_of_offset(dev, dev_of_offset(parent));

drivers/gpio/tegra_gpio.c
@@ -360,9 +360,9 @@ static int gpio_tegra_bind(struct udevice *parent)
 			plat->base_gpio = TEGRA_GPIOS_PER_PORT * base_port;
 			plat->port_name = gpio_port_name(base_port);
 
-			ret = device_bind_offset(parent, parent->driver,
-						 plat->port_name, plat, -1,
-						 &dev);
+			ret = device_bind(parent, parent->driver,
+					  plat->port_name, plat, ofnode_null(),
+					  &dev);
 			if (ret)
 				return ret;
 			dev_set_of_offset(dev, dev_of_offset(parent));

commit 785ddf1392173f67c81243d037a6ec0e80b1e78f

clk: at91: compat: partially revert "dm: Remove uses of device_bind_offset()"
Revert changes in at91 compat.c that cause u-boot to fail booting on
sama5d4_xplained and sama5d2_xplained

Log below:

<debug_uart>
No serial driver found
Could not initialize timer (err -19)

Could not initialize timer (err -19)

Could not initialize timer (err -19)

Could not initialize timer (err -19)

Could not initialize timer (err -19)

Could not initialize timer (err -19)

Could not initialize timer (err -19)

Could not initialize timer (err -19)

Fixes: 1ba1a9c7e0 ("dm: Remove uses of device_bind_offset()")
Cc: Simon Glass <sjg@chromium.org>
Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>
Reviewed-by: Simon Glass <sjg@chromium.org>

drivers/clk/at91/compat.c
@@ -62,30 +62,34 @@ static int at91_pmc_core_probe(struct udevice *dev)
  */
 int at91_clk_sub_device_bind(struct udevice *dev, const char *drv_name)
 {
-	ofnode parent = dev_ofnode(dev);
-	ofnode node;
+	const void *fdt = gd->fdt_blob;
+	int offset = dev_of_offset(dev);
 	bool pre_reloc_only = !(gd->flags & GD_FLG_RELOC);
 	const char *name;
 	int ret;
 
-	ofnode_for_each_subnode(node, parent) {
-		if (pre_reloc_only && !ofnode_pre_reloc(node))
+	for (offset = fdt_first_subnode(fdt, offset);
+	     offset > 0;
+	     offset = fdt_next_subnode(fdt, offset)) {
+		if (pre_reloc_only &&
+		    !ofnode_pre_reloc(offset_to_ofnode(offset)))
 			continue;
 		/*
 		 * If this node has "compatible" property, this is not
 		 * a clock sub-node, but a normal device. skip.
 		 */
-		if (ofnode_read_prop(node, "compatible", NULL))
+		fdt_get_property(fdt, offset, "compatible", &ret);
+		if (ret >= 0)
 			continue;
 
 		if (ret != -FDT_ERR_NOTFOUND)
 			return ret;
 
-		name = ofnode_get_name(node);
+		name = fdt_get_name(fdt, offset, NULL);
 		if (!name)
 			return -EINVAL;
-		ret = device_bind_driver_to_node(dev, drv_name, name, node,
-						 NULL);
+		ret = device_bind_driver_to_node(dev, drv_name, name,
+					offset_to_ofnode(offset), NULL);
 		if (ret)
 			return ret;
 	}