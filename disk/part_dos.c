ThingLabsOSS/superbird-uboot

commit 2328d92db81cd5e893c1775bd66483bd305a87b9

disk: part_dos: update partition table entries after write
Fixes issues when switching from GPT to MBR partition tables.

Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>
Acked-by: Marek Szyprowski <m.szyprowski@samsung.com>

disk/part_dos.c
@@ -423,6 +423,9 @@ int write_mbr_partitions(struct blk_desc *dev,
 		ext_part_sect = next_ebr;
 	}
 
+	/* Update the partition table entries*/
+	part_init(dev_desc);
+
 	return 0;
 }
 
@@ -498,6 +501,9 @@ int write_mbr_sector(struct blk_desc *dev_desc, void *buf)
 		       __func__, "MBR");
 		return 1;
 	}
+
+	/* Update the partition table entries*/
+	part_init(dev_desc);
 
 	return 0;
 }

commit d1b1bc78c6b7b6fd11a20d70ed82693f8d721e9e

disk/part_dos.c: Fix a variable typo in write_mbr_partitions()
This function is passed *dev not *dev_desc, so pass the right name to
part_init().

Fixes: 2328d92db81c ("disk: part_dos: update partition table entries after write")
Signed-off-by: Christian Melki <christian.melki@t2data.com>

disk/part_dos.c
@@ -424,7 +424,7 @@ int write_mbr_partitions(struct blk_desc *dev,
 	}
 
 	/* Update the partition table entries*/
-	part_init(dev_desc);
+	part_init(dev);
 
 	return 0;
 }