ThingLabsOSS/superbird-uboot

commit 668d0273f889dd4ac75159fcc150174a2233d859

phy: nop-phy: Fix enabling reset
The reset function should place the phy into reset, while the
init function should take the phy out of reset.  Currently the
reset function takes it out of reset, and the init calls the
reset.

Signed-off-by: Adam Ford <aford173@gmail.com>

drivers/phy/nop-phy.c
@@ -28,7 +28,7 @@ static int nop_phy_reset(struct phy *phy)
 	if (!dm_gpio_is_valid(&priv->reset_gpio))
 		return 0;
 
-	return dm_gpio_set_value(&priv->reset_gpio, false);
+	return dm_gpio_set_value(&priv->reset_gpio, true);
 }
 #endif
 
@@ -44,7 +44,8 @@ static int nop_phy_init(struct phy *phy)
 	}
 
 #if CONFIG_IS_ENABLED(DM_GPIO)
-	ret = nop_phy_reset(phy);
+	/* Take phy out of reset */
+	ret = dm_gpio_set_value(&priv->reset_gpio, false);
 	if (ret) {
 		if (CONFIG_IS_ENABLED(CLK))
 			clk_disable_bulk(&priv->bulk);

commit 8b0fa66882783bba9adcfeda58066f7ef5bb66be

phy: nop-phy: Fix phy reset if no reset-gpio defined
Ensure there is a valid reset-gpio defined before using it.

Fixes: 668d0273f889 ("phy: nop-phy: Fix enabling reset")
Cc: Adam Ford <aford173@gmail.com>
Signed-off-by: Tim Harvey <tharvey@gateworks.com>

drivers/phy/nop-phy.c
@@ -45,11 +45,13 @@ static int nop_phy_init(struct phy *phy)
 
 #if CONFIG_IS_ENABLED(DM_GPIO)
 	/* Take phy out of reset */
-	ret = dm_gpio_set_value(&priv->reset_gpio, false);
-	if (ret) {
-		if (CONFIG_IS_ENABLED(CLK))
-			clk_disable_bulk(&priv->bulk);
-		return ret;
+	if (dm_gpio_is_valid(&priv->reset_gpio)) {
+		ret = dm_gpio_set_value(&priv->reset_gpio, false);
+		if (ret) {
+			if (CONFIG_IS_ENABLED(CLK))
+				clk_disable_bulk(&priv->bulk);
+			return ret;
+		}
 	}
 #endif
 	return 0;