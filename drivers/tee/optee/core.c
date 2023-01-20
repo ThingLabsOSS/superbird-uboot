ThingLabsOSS/superbird-uboot

commit 950e8f2f981a66af77d0938b719945085f2af2e3

tee: optee: don't fail probe because of optee-rng
Fixes optee-rng driver bind sequence in optee driver to print a warning
message but not report an error status when a optee-rng service driver
fails to be bound as the optee driver itself is still fully functional.

Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

drivers/tee/optee/core.c
@@ -834,7 +834,7 @@ static int optee_probe(struct udevice *dev)
 		 */
 		ret = device_bind_driver(dev, "optee-rng", "optee-rng", NULL);
 		if (ret)
-			return ret;
+			dev_warn(dev, "ftpm_tee failed to bind: %d\n", ret);
 	}
 
 	return 0;

commit 04492f7b32356ae4c0ab239685e8054a10a96b20

tee: optee: fix a print error on rng probing
If we fail to probe the optee-rng device, we print a wrong message
referring to the firmware tpm.

Fixes: 950e8f2f981a ("tee: optee: don't fail probe because of optee-rng")
Reviewed-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Reviewed-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>
Signed-off-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

drivers/tee/optee/core.c
@@ -850,7 +850,7 @@ static int optee_probe(struct udevice *dev)
 		ret = device_bind_driver_to_node(dev, "optee-rng", "optee-rng",
 						 dev_ofnode(dev), NULL);
 		if (ret)
-			dev_warn(dev, "ftpm_tee failed to bind: %d\n", ret);
+			dev_warn(dev, "optee-rng failed to bind: %d\n", ret);
 	}
 
 	return 0;