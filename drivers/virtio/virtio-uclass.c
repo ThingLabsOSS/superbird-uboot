ThingLabsOSS/superbird-uboot

commit d2a54723abf2f5f6b11aa464a6286d1ff7c381b9

bootstd: Add a virtio bootdev
Add a bootdev for virtio so that these devices can be used with standard
boot.

Signed-off-by: Simon Glass <sjg@chromium.org>

drivers/virtio/virtio-uclass.c
@@ -18,6 +18,7 @@
 #define LOG_CATEGORY UCLASS_VIRTIO
 
 #include <common.h>
+#include <bootdev.h>
 #include <dm.h>
 #include <log.h>
 #include <malloc.h>
@@ -246,6 +247,12 @@ static int virtio_uclass_post_probe(struct udevice *udev)
 	}
 	device_set_name_alloced(vdev);
 
+	if (uc_priv->device == VIRTIO_ID_BLOCK) {
+		ret = bootdev_setup_for_dev(udev, name);
+		if (ret)
+			return log_msg_ret("bootdev", ret);
+	}
+
 	INIT_LIST_HEAD(&uc_priv->vqs);
 
 	return 0;
@@ -349,6 +356,26 @@ static int virtio_uclass_child_post_probe(struct udevice *vdev)
 	return 0;
 }
 
+static int virtio_bootdev_bind(struct udevice *dev)
+{
+	struct bootdev_uc_plat *ucp = dev_get_uclass_plat(dev);
+
+	ucp->prio = BOOTDEVP_2_SCAN_FAST;
+
+	return 0;
+}
+
+static int virtio_bootdev_hunt(struct bootdev_hunter *info, bool show)
+{
+	int ret;
+
+	ret = uclass_probe_all(UCLASS_VIRTIO);
+	if (ret && ret != -ENOENT)
+		return log_msg_ret("vir", ret);
+
+	return 0;
+}
+
 UCLASS_DRIVER(virtio) = {
 	.name	= "virtio",
 	.id	= UCLASS_VIRTIO,
@@ -359,4 +386,27 @@ UCLASS_DRIVER(virtio) = {
 	.child_pre_probe = virtio_uclass_child_pre_probe,
 	.child_post_probe = virtio_uclass_child_post_probe,
 	.per_device_auto	= sizeof(struct virtio_dev_priv),
+};
+
+struct bootdev_ops virtio_bootdev_ops = {
+};
+
+static const struct udevice_id virtio_bootdev_ids[] = {
+	{ .compatible = "u-boot,bootdev-virtio" },
+	{ }
+};
+
+U_BOOT_DRIVER(virtio_bootdev) = {
+	.name		= "virtio_bootdev",
+	.id		= UCLASS_BOOTDEV,
+	.ops		= &virtio_bootdev_ops,
+	.bind		= virtio_bootdev_bind,
+	.of_match	= virtio_bootdev_ids,
+};
+
+BOOTDEV_HUNTER(virtio_bootdev_hunter) = {
+	.prio		= BOOTDEVP_2_SCAN_FAST,
+	.uclass		= UCLASS_VIRTIO,
+	.hunt		= virtio_bootdev_hunt,
+	.drv		= DM_DRIVER_REF(virtio_bootdev),
 };

test/boot/bootdev.c
@@ -244,7 +244,8 @@ static int bootdev_test_hunter(struct unit_test_state *uts)
 	ut_assert_nextline("  30        nvme             nvme_bootdev");
 	ut_assert_nextline("  30        scsi             scsi_bootdev");
 	ut_assert_nextline("  40        usb              usb_bootdev");
-	ut_assert_nextline("(total hunters: 6)");
+	ut_assert_nextline("  30        virtio           virtio_bootdev");
+	ut_assert_nextline("(total hunters: 7)");
 	ut_assert_console_end();
 
 	ut_assertok(bootdev_hunt("usb1", false));
@@ -273,7 +274,7 @@ static int bootdev_test_cmd_hunt(struct unit_test_state *uts)
 	ut_assertok(run_command("bootdev hunt -l", 0));
 	ut_assert_nextline("Prio  Used  Uclass           Hunter");
 	ut_assert_nextlinen("----");
-	ut_assert_skip_to_line("(total hunters: 6)");
+	ut_assert_skip_to_line("(total hunters: 7)");
 	ut_assert_console_end();
 
 	/* Scan all hunters */
@@ -290,6 +291,7 @@ static int bootdev_test_cmd_hunt(struct unit_test_state *uts)
 	ut_assert_skip_to_line("Hunting with: usb");
 	ut_assert_nextline(
 		"Bus usb@1: scanning bus usb@1 for devices... 5 USB Device(s) found");
+	ut_assert_skip_to_line("Hunting with: virtio");
 	ut_assert_console_end();
 
 	/* List available hunters */
@@ -302,11 +304,11 @@ static int bootdev_test_cmd_hunt(struct unit_test_state *uts)
 	ut_assert_nextline("  30     *  nvme             nvme_bootdev");
 	ut_assert_nextline("  30     *  scsi             scsi_bootdev");
 	ut_assert_nextline("  40     *  usb              usb_bootdev");
-
-	ut_assert_nextline("(total hunters: 6)");
+	ut_assert_nextline("  30     *  virtio           virtio_bootdev");
+	ut_assert_nextline("(total hunters: 7)");
 	ut_assert_console_end();
 
-	ut_asserteq(GENMASK(5, 0), std->hunters_used);
+	ut_asserteq(GENMASK(6, 0), std->hunters_used);
 
 	return 0;
 }

commit 5fdd3b1930677d385b4c417ec23984e2adf109da

bootstd: Correct virtio block-device handling
At present virtio tries to attach QEMU services to a bootdev device, which
cannot work. Add a check for this.

Also use bootdev_setup_sibling_blk() to create the bootdev device, since
it allows the correct name to be used and bootdev_get_sibling_blk() to
work as expected.

The bootdev is not created on sandbox since it does have a real virtio
device and it is not possible to read blocks.

Signed-off-by: Simon Glass <sjg@chromium.org>
Fixes: d2a54723abf ("bootstd: Add a virtio bootdev")
Reported-by: Ilias Apalodimas <ilias.apalodimas@linaro.org>

drivers/virtio/virtio-uclass.c
@@ -247,8 +247,8 @@ static int virtio_uclass_post_probe(struct udevice *udev)
 	}
 	device_set_name_alloced(vdev);
 
-	if (uc_priv->device == VIRTIO_ID_BLOCK) {
-		ret = bootdev_setup_for_dev(udev, name);
+	if (uc_priv->device == VIRTIO_ID_BLOCK && !IS_ENABLED(CONFIG_SANDBOX)) {
+		ret = bootdev_setup_sibling_blk(vdev, "virtio_bootdev");
 		if (ret)
 			return log_msg_ret("bootdev", ret);
 	}
@@ -274,6 +274,10 @@ static int virtio_uclass_child_pre_probe(struct udevice *vdev)
 	u64 driver_features_legacy;
 	int i;
 	int ret;
+
+	/* bootdevs are not virtio devices */
+	if (device_get_uclass_id(vdev) == UCLASS_BOOTDEV)
+		return 0;
 
 	/*
 	 * Save the real virtio device (eg: virtio-net, virtio-blk) to