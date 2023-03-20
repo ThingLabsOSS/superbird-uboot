ThingLabsOSS/superbird-uboot

commit 97c9d810b50b74274264ef0400d04cf5bed480a7

clk: renesas: Add and enable CPG reset driver
Add trivial reset driver extension to the CPG clock driver. The change
turns current CPG UCLASS_CLK driver instance into an UCLASS_NOP proxy
driver, which in turn binds both generic rcar3_clk UCLASS_CLK clock
driver as well as generic rcar_rst UCLASS_RESET reset driver to the
CPG DT node. This way, any other drivers which use the 'reset' DT
property can now obtain valid reset handle backed by a reset driver.

The clock tables have been updated to represent the CPG driver and only
implement the generic CPG proxy driver bind call, which binds the clock
and reset drivers.

The DM_RESET is now enabled for all R-Car Gen3 platforms.

Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

drivers/clk/renesas/clk-rcar-gen3.c
@@ -13,12 +13,15 @@
 #include <common.h>
 #include <clk-uclass.h>
 #include <dm.h>
+#include <dm/device-internal.h>
+#include <dm/lists.h>
 #include <errno.h>
 #include <log.h>
 #include <wait_bit.h>
 #include <asm/global_data.h>
 #include <asm/io.h>
 #include <linux/bitops.h>
+#include <reset-uclass.h>
 
 #include <dt-bindings/clock/renesas-cpg-mssr.h>
 
@@ -389,7 +392,7 @@ const struct clk_ops gen3_clk_ops = {
 	.of_xlate	= gen3_clk_of_xlate,
 };
 
-int gen3_clk_probe(struct udevice *dev)
+static int gen3_clk_probe(struct udevice *dev)
 {
 	struct gen3_clk_priv *priv = dev_get_priv(dev);
 	struct cpg_mssr_info *info =
@@ -447,9 +450,84 @@ int gen3_clk_probe(struct udevice *dev)
 	return 0;
 }
 
-int gen3_clk_remove(struct udevice *dev)
+static int gen3_clk_remove(struct udevice *dev)
 {
 	struct gen3_clk_priv *priv = dev_get_priv(dev);
 
 	return renesas_clk_remove(priv->base, priv->info);
+}
+
+U_BOOT_DRIVER(clk_gen3) = {
+	.name		= "clk_gen3",
+	.id		= UCLASS_CLK,
+	.priv_auto	= sizeof(struct gen3_clk_priv),
+	.ops		= &gen3_clk_ops,
+	.probe		= gen3_clk_probe,
+	.remove		= gen3_clk_remove,
+};
+
+static int gen3_reset_assert(struct reset_ctl *reset_ctl)
+{
+	struct udevice *cdev = (struct udevice *)dev_get_driver_data(reset_ctl->dev);
+	struct gen3_clk_priv *priv = dev_get_priv(cdev);
+	unsigned int reg = reset_ctl->id / 32;
+	unsigned int bit = reset_ctl->id % 32;
+	u32 bitmask = BIT(bit);
+
+	writel(bitmask, priv->base + priv->info->reset_regs[reg]);
+
+	return 0;
+}
+
+static int gen3_reset_deassert(struct reset_ctl *reset_ctl)
+{
+	struct udevice *cdev = (struct udevice *)dev_get_driver_data(reset_ctl->dev);
+	struct gen3_clk_priv *priv = dev_get_priv(cdev);
+	unsigned int reg = reset_ctl->id / 32;
+	unsigned int bit = reset_ctl->id % 32;
+	u32 bitmask = BIT(bit);
+
+	writel(bitmask, priv->base + priv->info->reset_clear_regs[reg]);
+
+	return 0;
+}
+
+static const struct reset_ops rst_gen3_ops = {
+	.rst_assert = gen3_reset_assert,
+	.rst_deassert = gen3_reset_deassert,
+};
+
+U_BOOT_DRIVER(rst_gen3) = {
+	.name = "rst_gen3",
+	.id = UCLASS_RESET,
+	.ops = &rst_gen3_ops,
+};
+
+int gen3_cpg_bind(struct udevice *parent)
+{
+	struct cpg_mssr_info *info =
+		(struct cpg_mssr_info *)dev_get_driver_data(parent);
+	struct udevice *cdev, *rdev;
+	struct driver *drv;
+	int ret;
+
+	drv = lists_driver_lookup_name("clk_gen3");
+	if (!drv)
+		return -ENOENT;
+
+	ret = device_bind_with_driver_data(parent, drv, "clk_gen3", (ulong)info,
+					   dev_ofnode(parent), &cdev);
+	if (ret)
+		return ret;
+
+	drv = lists_driver_lookup_name("rst_gen3");
+	if (!drv)
+		return -ENOENT;
+
+	ret = device_bind_with_driver_data(parent, drv, "rst_gen3", (ulong)cdev,
+					   dev_ofnode(parent), &rdev);
+	if (ret)
+		device_unbind(cdev);
+
+	return ret;
 }

arch/arm/mach-rmobile/Kconfig.64
@@ -201,4 +201,7 @@ config MULTI_DTB_FIT_USER_DEF_ADDR
 config SYS_MALLOC_F_LEN
 	default 0x8000 if RCAR_GEN3
 
+config DM_RESET
+	default y if RCAR_GEN3
+
 endif

drivers/clk/renesas/r8a774a1-cpg-mssr.c
@@ -331,7 +331,7 @@ static const struct cpg_mssr_info r8a774a1_cpg_mssr_info = {
 	.get_pll_config		= r8a774a1_get_pll_config,
 };
 
-static const struct udevice_id r8a774a1_clk_ids[] = {
+static const struct udevice_id r8a774a1_cpg_ids[] = {
 	{
 		.compatible	= "renesas,r8a774a1-cpg-mssr",
 		.data		= (ulong)&r8a774a1_cpg_mssr_info,
@@ -339,12 +339,9 @@ static const struct udevice_id r8a774a1_clk_ids[] = {
 	{ }
 };
 
-U_BOOT_DRIVER(clk_r8a774a1) = {
-	.name		= "clk_r8a774a1",
-	.id		= UCLASS_CLK,
-	.of_match	= r8a774a1_clk_ids,
-	.priv_auto	= sizeof(struct gen3_clk_priv),
-	.ops		= &gen3_clk_ops,
-	.probe		= gen3_clk_probe,
-	.remove		= gen3_clk_remove,
+U_BOOT_DRIVER(cpg_r8a774a1) = {
+	.name		= "cpg_r8a774a1",
+	.id		= UCLASS_NOP,
+	.of_match	= r8a774a1_cpg_ids,
+	.bind		= gen3_cpg_bind,
 };

drivers/clk/renesas/r8a774b1-cpg-mssr.c
@@ -329,7 +329,7 @@ static const struct cpg_mssr_info r8a774b1_cpg_mssr_info = {
 	.get_pll_config		= r8a774b1_get_pll_config,
 };
 
-static const struct udevice_id r8a774b1_clk_ids[] = {
+static const struct udevice_id r8a774b1_cpg_ids[] = {
 	{
 		.compatible	= "renesas,r8a774b1-cpg-mssr",
 		.data		= (ulong)&r8a774b1_cpg_mssr_info,
@@ -337,12 +337,9 @@ static const struct udevice_id r8a774b1_clk_ids[] = {
 	{ }
 };
 
-U_BOOT_DRIVER(clk_r8a774b1) = {
-	.name		= "clk_r8a774b1",
-	.id		= UCLASS_CLK,
-	.of_match	= r8a774b1_clk_ids,
-	.priv_auto	= sizeof(struct gen3_clk_priv),
-	.ops		= &gen3_clk_ops,
-	.probe		= gen3_clk_probe,
-	.remove		= gen3_clk_remove,
+U_BOOT_DRIVER(cpg_r8a774b1) = {
+	.name		= "cpg_r8a774b1",
+	.id		= UCLASS_NOP,
+	.of_match	= r8a774b1_cpg_ids,
+	.bind		= gen3_cpg_bind,
 };

drivers/clk/renesas/r8a774c0-cpg-mssr.c
@@ -300,7 +300,7 @@ const struct cpg_mssr_info r8a774c0_cpg_mssr_info = {
 	.get_pll_config		= r8a774c0_get_pll_config,
 };
 
-static const struct udevice_id r8a774c0_clk_ids[] = {
+static const struct udevice_id r8a774c0_cpg_ids[] = {
 	{
 		.compatible	= "renesas,r8a774c0-cpg-mssr",
 		.data		= (ulong)&r8a774c0_cpg_mssr_info
@@ -308,12 +308,9 @@ static const struct udevice_id r8a774c0_clk_ids[] = {
 	{ }
 };
 
-U_BOOT_DRIVER(clk_r8a774c0) = {
-	.name		= "clk_r8a774c0",
-	.id		= UCLASS_CLK,
-	.of_match	= r8a774c0_clk_ids,
-	.priv_auto	= sizeof(struct gen3_clk_priv),
-	.ops		= &gen3_clk_ops,
-	.probe		= gen3_clk_probe,
-	.remove		= gen3_clk_remove,
+U_BOOT_DRIVER(cpg_r8a774c0) = {
+	.name		= "cpg_r8a774c0",
+	.id		= UCLASS_NOP,
+	.of_match	= r8a774c0_cpg_ids,
+	.bind		= gen3_cpg_bind,
 };

drivers/clk/renesas/r8a774e1-cpg-mssr.c
@@ -343,7 +343,7 @@ static const struct cpg_mssr_info r8a774e1_cpg_mssr_info = {
 	.get_pll_config		= r8a774e1_get_pll_config,
 };
 
-static const struct udevice_id r8a774e1_clk_ids[] = {
+static const struct udevice_id r8a774e1_cpg_ids[] = {
 	{
 		.compatible	= "renesas,r8a774e1-cpg-mssr",
 		.data		= (ulong)&r8a774e1_cpg_mssr_info
@@ -351,12 +351,9 @@ static const struct udevice_id r8a774e1_clk_ids[] = {
 	{ }
 };
 
-U_BOOT_DRIVER(clk_r8a774e1) = {
-	.name		= "clk_r8a774e1",
-	.id		= UCLASS_CLK,
-	.of_match	= r8a774e1_clk_ids,
-	.priv_auto	= sizeof(struct gen3_clk_priv),
-	.ops		= &gen3_clk_ops,
-	.probe		= gen3_clk_probe,
-	.remove		= gen3_clk_remove,
+U_BOOT_DRIVER(cpg_r8a774e1) = {
+	.name		= "cpg_r8a774e1",
+	.id		= UCLASS_NOP,
+	.of_match	= r8a774e1_cpg_ids,
+	.bind		= gen3_cpg_bind,
 };

drivers/clk/renesas/r8a7795-cpg-mssr.c
@@ -380,7 +380,7 @@ static const struct cpg_mssr_info r8a7795_cpg_mssr_info = {
 	.get_pll_config		= r8a7795_get_pll_config,
 };
 
-static const struct udevice_id r8a7795_clk_ids[] = {
+static const struct udevice_id r8a7795_cpg_ids[] = {
 	{
 		.compatible	= "renesas,r8a7795-cpg-mssr",
 		.data		= (ulong)&r8a7795_cpg_mssr_info
@@ -388,12 +388,9 @@ static const struct udevice_id r8a7795_clk_ids[] = {
 	{ }
 };
 
-U_BOOT_DRIVER(clk_r8a7795) = {
-	.name		= "clk_r8a7795",
-	.id		= UCLASS_CLK,
-	.of_match	= r8a7795_clk_ids,
-	.priv_auto	= sizeof(struct gen3_clk_priv),
-	.ops		= &gen3_clk_ops,
-	.probe		= gen3_clk_probe,
-	.remove		= gen3_clk_remove,
+U_BOOT_DRIVER(cpg_r8a7795) = {
+	.name		= "cpg_r8a7795",
+	.id		= UCLASS_NOP,
+	.of_match	= r8a7795_cpg_ids,
+	.bind		= gen3_cpg_bind,
 };

drivers/clk/renesas/r8a7796-cpg-mssr.c
@@ -370,7 +370,7 @@ static const struct cpg_mssr_info r8a77961_cpg_mssr_info = {
 	.get_pll_config		= r8a7796_get_pll_config,
 };
 
-static const struct udevice_id r8a7796_clk_ids[] = {
+static const struct udevice_id r8a7796_cpg_ids[] = {
 	{
 		.compatible	= "renesas,r8a7796-cpg-mssr",
 		.data		= (ulong)&r8a7796_cpg_mssr_info,
@@ -382,12 +382,9 @@ static const struct udevice_id r8a7796_clk_ids[] = {
 	{ }
 };
 
-U_BOOT_DRIVER(clk_r8a7796) = {
-	.name		= "clk_r8a7796",
-	.id		= UCLASS_CLK,
-	.of_match	= r8a7796_clk_ids,
-	.priv_auto	= sizeof(struct gen3_clk_priv),
-	.ops		= &gen3_clk_ops,
-	.probe		= gen3_clk_probe,
-	.remove		= gen3_clk_remove,
+U_BOOT_DRIVER(cpg_r8a7796) = {
+	.name		= "cpg_r8a7796",
+	.id		= UCLASS_NOP,
+	.of_match	= r8a7796_cpg_ids,
+	.bind		= gen3_cpg_bind,
 };

commit 42f5ce2074efc7d7ae6fe15872e0f489c1fd97f9

clk: renesas: Pack reset identifier before look up
The reset identifier must be processed via MOD_CLK_PACK() before it is
used to look up register and bit within reset_regs or reset_clear_regs
arrays, otherwise completely bogus register and bit is picked from the
arrays, one which may even be out of range.

Fixes: 97c9d810b50 ("clk: renesas: Add and enable CPG reset driver")
Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

drivers/clk/renesas/clk-rcar-gen3.c
@@ -479,8 +479,9 @@ static int gen3_reset_assert(struct reset_ctl *reset_ctl)
 {
 	struct udevice *cdev = (struct udevice *)dev_get_driver_data(reset_ctl->dev);
 	struct gen3_clk_priv *priv = dev_get_priv(cdev);
-	unsigned int reg = reset_ctl->id / 32;
-	unsigned int bit = reset_ctl->id % 32;
+	unsigned int packed_id = MOD_CLK_PACK(reset_ctl->id);
+	unsigned int reg = packed_id / 32;
+	unsigned int bit = packed_id % 32;
 	u32 bitmask = BIT(bit);
 
 	writel(bitmask, priv->base + priv->info->reset_regs[reg]);
@@ -492,8 +493,9 @@ static int gen3_reset_deassert(struct reset_ctl *reset_ctl)
 {
 	struct udevice *cdev = (struct udevice *)dev_get_driver_data(reset_ctl->dev);
 	struct gen3_clk_priv *priv = dev_get_priv(cdev);
-	unsigned int reg = reset_ctl->id / 32;
-	unsigned int bit = reset_ctl->id % 32;
+	unsigned int packed_id = MOD_CLK_PACK(reset_ctl->id);
+	unsigned int reg = packed_id / 32;
+	unsigned int bit = packed_id % 32;
 	u32 bitmask = BIT(bit);
 
 	writel(bitmask, priv->base + priv->info->reset_clear_regs[reg]);