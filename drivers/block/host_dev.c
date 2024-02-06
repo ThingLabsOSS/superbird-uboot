ThingLabsOSS/superbird-uboot

commit a8bcb2106a181684edb3578575826e8a647e9af8

blk: host_dev: Sanity check on the size of host backing file
Since we are emulating a block device, its size should be multiple
of the configured block size.

Signed-off-by: Bin Meng <bmeng@tinylab.org>
Reviewed-by: Simon Glass <sjg@chromium.org>

drivers/block/host_dev.c
@@ -58,6 +58,11 @@ static int host_sb_attach_file(struct udevice *dev, const char *filename)
 
 	size = os_filesize(fd);
 	desc = dev_get_uclass_plat(blk);
+	if (size % desc->blksz) {
+		printf("The size of host backing file '%s' is not multiple of "
+		       "the device block size\n", filename);
+		goto err_fname;
+	}
 	desc->lba = size / desc->blksz;
 
 	/* write this in last, when nothing can go wrong */

commit d16872ec2b1f6262bf7e170ba4dedeb9c9a838fb

blk: host_dev: Fix error code in host_sb_attach_file()
This error path should return -EINVAL instead of success.

Fixes: a8bcb2106a18 ("blk: host_dev: Sanity check on the size of host backing file")
Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

drivers/block/host_dev.c
@@ -61,6 +61,7 @@ static int host_sb_attach_file(struct udevice *dev, const char *filename)
 	if (size % desc->blksz) {
 		printf("The size of host backing file '%s' is not multiple of "
 		       "the device block size\n", filename);
+		ret = -EINVAL;
 		goto err_fname;
 	}
 	desc->lba = size / desc->blksz;