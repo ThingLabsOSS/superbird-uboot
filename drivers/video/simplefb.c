RaitoBezarius/uboot-nwa50ax

commit 41c753e6e21a8ce831139ca090f347f78d7aac0c

video: simplefb
Not really qcom specific, but for now qcom/lk is the one firmware that
is (afaiu) setting up the appropriate dt node for pre-configured
display.  Uses the generic simple-framebuffer DT bindings so this should
be useful on other platforms.

Signed-off-by: Rob Clark <robdclark@gmail.com>

drivers/video/simplefb.c
@@ -1,0 +1,73 @@
+/*
+ * (C) Copyright 2017 Rob Clark
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <dm.h>
+#include <fdtdec.h>
+#include <fdt_support.h>
+#include <video.h>
+
+static int simple_video_probe(struct udevice *dev)
+{
+	struct video_uc_platdata *plat = dev_get_uclass_platdata(dev);
+	struct video_priv *uc_priv = dev_get_uclass_priv(dev);
+	const void *blob = gd->fdt_blob;
+	const int node = dev_of_offset(dev);
+	const char *format;
+	fdt_addr_t base;
+	fdt_size_t size;
+
+	base = fdtdec_get_addr_size_auto_parent(blob, dev_of_offset(dev->parent),
+			node, "reg", 0, &size, false);
+	if (base == FDT_ADDR_T_NONE) {
+		debug("%s: Failed to decode memory region\n", __func__);
+		return -EINVAL;
+	}
+
+	debug("%s: base=%llx, size=%llu\n", __func__, base, size);
+
+	/*
+	 * TODO is there some way to reserve the framebuffer
+	 * region so it isn't clobbered?
+	 */
+	plat->base = base;
+	plat->size = size;
+
+	video_set_flush_dcache(dev, true);
+
+	debug("%s: Query resolution...\n", __func__);
+
+	uc_priv->xsize = fdtdec_get_uint(blob, node, "width", 0);
+	uc_priv->ysize = fdtdec_get_uint(blob, node, "height", 0);
+	uc_priv->rot = 0;
+
+	format = fdt_getprop(blob, node, "format", NULL);
+	debug("%s: %dx%d@%s\n", __func__, uc_priv->xsize, uc_priv->ysize, format);
+
+	if (strcmp(format, "r5g6b5") == 0) {
+		uc_priv->bpix = VIDEO_BPP16;
+	} else if (strcmp(format, "a8b8g8r8") == 0) {
+		uc_priv->bpix = VIDEO_BPP32;
+	} else {
+		printf("%s: invalid format: %s\n", __func__, format);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static const struct udevice_id simple_video_ids[] = {
+	{ .compatible = "simple-framebuffer" },
+	{ }
+};
+
+U_BOOT_DRIVER(simple_video) = {
+	.name	= "simple_video",
+	.id	= UCLASS_VIDEO,
+	.of_match = simple_video_ids,
+	.probe	= simple_video_probe,
+	.flags	= DM_FLAG_PRE_RELOC,
+};

drivers/video/Kconfig
@@ -624,4 +624,14 @@ config VIDEO_DW_HDMI
 	  rather requires a SoC-specific glue driver to call it), it
 	  can not be enabled from the configuration menu.
 
+config VIDEO_SIMPLE
+	bool "Simple display driver for preconfigured display"
+	help
+	  Enables a simple generic display driver which utilizes the
+	  simple-framebuffer devicetree bindings.
+
+	  This driver assumes that the display hardware has been initialized
+	  before u-boot starts, and u-boot will simply render to the pre-
+	  allocated frame buffer surface.
+
 endmenu

drivers/video/Makefile
@@ -52,7 +52,7 @@ obj-$(CONFIG_FORMIKE) += formike.o
 obj-$(CONFIG_LG4573) += lg4573.o
 obj-$(CONFIG_AM335X_LCD) += am335x-fb.o
 obj-$(CONFIG_VIDEO_DW_HDMI) += dw_hdmi.o
-
+obj-$(CONFIG_VIDEO_SIMPLE) += simplefb.o
 obj-${CONFIG_VIDEO_TEGRA124} += tegra124/
 obj-${CONFIG_EXYNOS_FB} += exynos/
 obj-${CONFIG_VIDEO_ROCKCHIP} += rockchip/

ThingLabsOSS/superbird-uboot

commit 23e064953ad62877aaab31a791a0d5fb11dcb2f3

video: simplefb: modernise DT parsing
simplefb was using old style FDT parsing which doesn't behave well in
combination with livetree. Update it to use ofnode instead and add a
missing null check for the "format" property.

Standardise the error logging while we're here.

Fixes: 41c753e6e21a ("video: simplefb")
Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

drivers/video/simplefb.c
@@ -15,14 +15,14 @@ static int simple_video_probe(struct udevice *dev)
 {
 	struct video_uc_plat *plat = dev_get_uclass_plat(dev);
 	struct video_priv *uc_priv = dev_get_uclass_priv(dev);
-	const void *blob = gd->fdt_blob;
-	const int node = dev_of_offset(dev);
+	ofnode node = dev_ofnode(dev);
 	const char *format;
+	int ret;
 	fdt_addr_t base;
 	fdt_size_t size;
+	u32 width, height, rot;
 
-	base = fdtdec_get_addr_size_auto_parent(blob, dev_of_offset(dev->parent),
-			node, "reg", 0, &size, false);
+	base = dev_read_addr_size(dev, &size);
 	if (base == FDT_ADDR_T_NONE) {
 		debug("%s: Failed to decode memory region\n", __func__);
 		return -EINVAL;
@@ -41,16 +41,24 @@ static int simple_video_probe(struct udevice *dev)
 
 	debug("%s: Query resolution...\n", __func__);
 
-	uc_priv->xsize = fdtdec_get_uint(blob, node, "width", 0);
-	uc_priv->ysize = fdtdec_get_uint(blob, node, "height", 0);
-	uc_priv->rot = fdtdec_get_uint(blob, node, "rot", 0);
-	if (uc_priv->rot > 3) {
-		log_debug("%s: invalid rot\n", __func__);
-		return log_msg_ret("rot", -EINVAL);
+	ret = ofnode_read_u32(node, "width", &width);
+	ret = ret ?: ofnode_read_u32(node, "height", &height);
+	if (ret || !width || !height) {
+		log_err("%s: invalid width or height: %d\n", __func__, ret);
+		return ret ?: -EINVAL;
 	}
+	ofnode_read_u32(node, "rot", &rot);
+	uc_priv->rot = rot;
+	uc_priv->xsize = width;
+	uc_priv->ysize = height;
 
-	format = fdt_getprop(blob, node, "format", NULL);
+	format = ofnode_read_string(node, "format");
 	debug("%s: %dx%d@%s\n", __func__, uc_priv->xsize, uc_priv->ysize, format);
+
+	if (!format) {
+		log_err("%s: please add required property \"format\"\n", __func__);
+		return -EINVAL;
+	}
 
 	if (strcmp(format, "r5g6b5") == 0) {
 		uc_priv->bpix = VIDEO_BPP16;
@@ -67,7 +75,7 @@ static int simple_video_probe(struct udevice *dev)
 		uc_priv->bpix = VIDEO_BPP32;
 		uc_priv->format = VIDEO_X2R10G10B10;
 	} else {
-		printf("%s: invalid format: %s\n", __func__, format);
+		log_err("%s: invalid format: %s\n", __func__, format);
 		return -EINVAL;
 	}
 