ThingLabsOSS/superbird-uboot

commit d02ec7af4b765a8ba383cf8d7872beae702dd6ea

moveconfig: Rename the tool to qconfig
This does not move configs anymore, but queries them, based on a database
it can build. Rename the tool to better reflect its purpose.

Signed-off-by: Simon Glass <sjg@chromium.org>

tools/qconfig.py
@@ -5,7 +5,7 @@
 #
 
 """
-Move config options from headers to defconfig files.
+Build and query a Kconfig database for boards.
 
 See doc/develop/moveconfig.rst for documentation.
 """
@@ -900,7 +900,7 @@ def do_imply_config(config_list, add_imply, imply_flags, skip_added,
     all x86 boards will have that option, avoiding adding CONFIG_CMD_IRQ to
     each of the x86 defconfig files.
 
-    This function uses the moveconfig database to find such options. It
+    This function uses the qconfig database to find such options. It
     displays a list of things that could possibly imply those in the list.
     The algorithm ignores any that start with CONFIG_TARGET since these
     typically refer to only a few defconfigs (often one). It also does not

commit 3e13a1d33ee95fcd4b9a1973372eff70496e10e8

qconfig: Rename the doc link
This was missed during the renaming of the tool. Fix it.

Signed-off-by: Simon Glass <sjg@chromium.org>
Fixes: d02ec7af4b7 ("moveconfig: Rename the tool to qconfig")

tools/qconfig.py
@@ -3,7 +3,7 @@
 
 """Build and query a Kconfig database for boards.
 
-See doc/develop/moveconfig.rst for documentation.
+See doc/develop/qconfig.rst for documentation.
 
 Author: Masahiro Yamada <yamada.masahiro@socionext.com>
 Author: Simon Glass <sjg@chromium.org>