ThingLabsOSS/superbird-uboot

commit 9b3147210ba5de526bf8fb39a1ebf708db695573

net: designware: Allocate bb_miiphy using bb_miiphy_alloc() and fill in callbacks
Allocate bb_miiphy using bb_miiphy_alloc() and fill in callbacks
currently listed in bb_miiphy_buses[] array. This is a temporary
duplication of assignment to avoid breakage, which will be removed
in follow up patches. At this point, the bb_miiphy callbacks can
reach these accessors by doing container_of() on struct mii_dev.

Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
Reviewed-by: Paul Barker <paul.barker.ct@bp.renesas.com>

drivers/net/designware.c
@@ -307,13 +307,16 @@ int bb_miiphy_buses_num = ARRAY_SIZE(bb_miiphy_buses);
 static int dw_bb_mdio_init(const char *name, struct udevice *dev)
 {
 	struct dw_eth_dev *dwpriv = dev_get_priv(dev);
-	struct mii_dev *bus = mdio_alloc();
+	struct bb_miiphy_bus *bb_miiphy = bb_miiphy_alloc();
+	struct mii_dev *bus;
 	int ret;
 
-	if (!bus) {
+	if (!bb_miiphy) {
 		printf("Failed to allocate MDIO bus\n");
 		return -ENOMEM;
 	}
+
+	bus = &bb_miiphy->mii;
 
 	debug("\n%s: use bitbang mii..\n", dev->name);
 	ret = gpio_request_by_name(dev, "snps,mdc-gpio", 0,
@@ -344,6 +347,15 @@ static int dw_bb_mdio_init(const char *name, struct udevice *dev)
 	bus->reset = dw_mdio_reset;
 #endif
 	bus->priv = dwpriv;
+
+	/* Copy the bus accessors, name and private data */
+	bb_miiphy->mdio_active = dw_eth_bb_mdio_active;
+	bb_miiphy->mdio_tristate = dw_eth_bb_mdio_tristate;
+	bb_miiphy->set_mdio = dw_eth_bb_set_mdio;
+	bb_miiphy->get_mdio = dw_eth_bb_get_mdio;
+	bb_miiphy->set_mdc = dw_eth_bb_set_mdc;
+	bb_miiphy->delay = dw_eth_bb_delay;
+	strlcpy(bb_miiphy->name, bus->name, MDIO_NAME_LEN);
 
 	return mdio_register(bus);
 }
@@ -968,7 +980,12 @@ int designware_eth_probe(struct udevice *dev)
 	/* continue here for cleanup if no PHY found */
 	err = ret;
 	mdio_unregister(priv->bus);
-	mdio_free(priv->bus);
+#if IS_ENABLED(CONFIG_BITBANGMII) && IS_ENABLED(CONFIG_DM_GPIO)
+	if (bbmiiphy)
+		bb_miiphy_free(container_of(priv->bus, struct bb_miiphy_bus, mii));
+	else
+#endif
+		mdio_free(priv->bus);
 mdio_err:
 
 #ifdef CONFIG_CLK

commit 809b07a727ebf124fa9dde5654fc6e9ed30a5e99

net: designware: Switch back to mdio_alloc()
Use mdio_alloc() again to allocate MDIO bus. This is possible
because all the miiphybb parameters and ops passing is handled in
at bb_miiphy_read()/bb_miiphy_write() level.

This also fixes previously missed bb_miiphy_free() in .remove
callback of this driver. which does not pose a problem anymore.

Fixes: 9b3147210ba5 ("net: designware: Allocate bb_miiphy using bb_miiphy_alloc() and fill in callbacks")
Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
Reviewed-by: Paul Barker <paul.barker.ct@bp.renesas.com>

drivers/net/designware.c
@@ -316,16 +316,13 @@ static int dw_bb_miiphy_write(struct mii_dev *miidev, int addr,
 static int dw_bb_mdio_init(const char *name, struct udevice *dev)
 {
 	struct dw_eth_dev *dwpriv = dev_get_priv(dev);
-	struct bb_miiphy_bus *bb_miiphy = bb_miiphy_alloc();
-	struct mii_dev *bus;
+	struct mii_dev *bus = mdio_alloc();
 	int ret;
 
-	if (!bb_miiphy) {
+	if (!bus) {
 		printf("Failed to allocate MDIO bus\n");
 		return -ENOMEM;
 	}
-
-	bus = &bb_miiphy->mii;
 
 	debug("\n%s: use bitbang mii..\n", dev->name);
 	ret = gpio_request_by_name(dev, "snps,mdc-gpio", 0,
@@ -855,7 +852,6 @@ int designware_eth_probe(struct udevice *dev)
 {
 	struct eth_pdata *pdata = dev_get_plat(dev);
 	struct dw_eth_dev *priv = dev_get_priv(dev);
-	bool __maybe_unused bbmiiphy = false;
 	phys_addr_t iobase = pdata->iobase;
 	void *ioaddr;
 	int ret, err;
@@ -947,8 +943,7 @@ int designware_eth_probe(struct udevice *dev)
 	priv->max_speed = pdata->max_speed;
 
 #if IS_ENABLED(CONFIG_BITBANGMII) && IS_ENABLED(CONFIG_DM_GPIO)
-	bbmiiphy = dev_read_bool(dev, "snps,bitbang-mii");
-	if (bbmiiphy) {
+	if (dev_read_bool(dev, "snps,bitbang-mii")) {
 		ret = dw_bb_mdio_init(dev->name, dev);
 		if (ret) {
 			err = ret;
@@ -978,12 +973,7 @@ int designware_eth_probe(struct udevice *dev)
 	/* continue here for cleanup if no PHY found */
 	err = ret;
 	mdio_unregister(priv->bus);
-#if IS_ENABLED(CONFIG_BITBANGMII) && IS_ENABLED(CONFIG_DM_GPIO)
-	if (bbmiiphy)
-		bb_miiphy_free(container_of(priv->bus, struct bb_miiphy_bus, mii));
-	else
-#endif
-		mdio_free(priv->bus);
+	mdio_free(priv->bus);
 mdio_err:
 
 #ifdef CONFIG_CLK