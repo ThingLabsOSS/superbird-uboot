ThingLabsOSS/superbird-uboot

commit 32470102409bcbb9c88954ef2f4277f1417fc3fa

firmware: scmi: voltage regulator
Implement voltage regulators interfaced by the SCMI voltage domain
protocol. The DT bindings are defined in the Linux kernel since
SCMI voltage domain and regulators patches [1] and [2] integration
in v5.11-rc7.

Link: [1] https://github.com/torvalds/linux/commit/0f80fcec08e9c50b8d2992cf26495673765ebaba
Link: [2] https://github.com/torvalds/linux/commit/2add5cacff3531e54c50b0832128299faa9f0563
Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jaehoon Chung <jh80.chung@samsung.com>

drivers/firmware/scmi/scmi_agent-uclass.c
@@ -80,6 +80,16 @@ static int scmi_bind_protocols(struct udevice *dev)
 			if (IS_ENABLED(CONFIG_RESET_SCMI))
 				drv = DM_DRIVER_GET(scmi_reset_domain);
 			break;
+		case SCMI_PROTOCOL_ID_VOLTAGE_DOMAIN:
+			if (IS_ENABLED(CONFIG_DM_REGULATOR_SCMI)) {
+				node = ofnode_find_subnode(node, "regulators");
+				if (!ofnode_valid(node)) {
+					dev_err(dev, "no regulators node\n");
+					return -ENXIO;
+				}
+				drv = DM_DRIVER_GET(scmi_voltage_domain);
+			}
+			break;
 		default:
 			break;
 		}

drivers/power/regulator/scmi_regulator.c
@@ -1,0 +1,195 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (C) 2020-2021 Linaro Limited
+ */
+#include <common.h>
+#include <dm.h>
+#include <errno.h>
+#include <scmi_agent.h>
+#include <scmi_protocols.h>
+#include <asm/types.h>
+#include <dm/device.h>
+#include <dm/device_compat.h>
+#include <dm/device-internal.h>
+#include <linux/kernel.h>
+#include <power/regulator.h>
+
+/**
+ * struct scmi_regulator_platdata - Platform data for a scmi voltage domain regulator
+ * @domain_id: ID representing the regulator for the related SCMI agent
+ */
+struct scmi_regulator_platdata {
+	u32 domain_id;
+};
+
+static int scmi_voltd_set_enable(struct udevice *dev, bool enable)
+{
+	struct scmi_regulator_platdata *pdata = dev_get_plat(dev);
+	struct scmi_voltd_config_set_in in = {
+		.domain_id = pdata->domain_id,
+		.config = enable ? SCMI_VOLTD_CONFIG_ON : SCMI_VOLTD_CONFIG_OFF,
+	};
+	struct scmi_voltd_config_set_out out;
+	struct scmi_msg msg = SCMI_MSG_IN(SCMI_PROTOCOL_ID_VOLTAGE_DOMAIN,
+					  SCMI_VOLTAGE_DOMAIN_CONFIG_SET,
+					  in, out);
+	int ret;
+
+	ret = devm_scmi_process_msg(dev->parent->parent, &msg);
+	if (ret)
+		return ret;
+
+	ret = scmi_to_linux_errno(out.status);
+	if (ret)
+		return ret;
+
+	return ret;
+}
+
+static int scmi_voltd_get_enable(struct udevice *dev)
+{
+	struct scmi_regulator_platdata *pdata = dev_get_plat(dev);
+	struct scmi_voltd_config_get_in in = {
+		.domain_id = pdata->domain_id,
+	};
+	struct scmi_voltd_config_get_out out;
+	struct scmi_msg msg = SCMI_MSG_IN(SCMI_PROTOCOL_ID_VOLTAGE_DOMAIN,
+					  SCMI_VOLTAGE_DOMAIN_CONFIG_GET,
+					  in, out);
+	int ret;
+
+	ret = devm_scmi_process_msg(dev->parent->parent, &msg);
+	if (ret < 0)
+		return ret;
+
+	ret = scmi_to_linux_errno(out.status);
+	if (ret < 0)
+		return ret;
+
+	return out.config == SCMI_VOLTD_CONFIG_ON;
+}
+
+static int scmi_voltd_set_voltage_level(struct udevice *dev, int uV)
+{
+	struct scmi_regulator_platdata *pdata = dev_get_plat(dev);
+	struct scmi_voltd_level_set_in in = {
+		.domain_id = pdata->domain_id,
+		.voltage_level = uV,
+	};
+	struct scmi_voltd_level_set_out out;
+	struct scmi_msg msg = SCMI_MSG_IN(SCMI_PROTOCOL_ID_VOLTAGE_DOMAIN,
+					  SCMI_VOLTAGE_DOMAIN_LEVEL_SET,
+					  in, out);
+	int ret;
+
+	ret = devm_scmi_process_msg(dev->parent->parent, &msg);
+	if (ret < 0)
+		return ret;
+
+	return scmi_to_linux_errno(out.status);
+}
+
+static int scmi_voltd_get_voltage_level(struct udevice *dev)
+{
+	struct scmi_regulator_platdata *pdata = dev_get_plat(dev);
+	struct scmi_voltd_level_get_in in = {
+		.domain_id = pdata->domain_id,
+	};
+	struct scmi_voltd_level_get_out out;
+	struct scmi_msg msg = SCMI_MSG_IN(SCMI_PROTOCOL_ID_VOLTAGE_DOMAIN,
+					  SCMI_VOLTAGE_DOMAIN_LEVEL_GET,
+					  in, out);
+	int ret;
+
+	ret = devm_scmi_process_msg(dev->parent->parent, &msg);
+	if (ret < 0)
+		return ret;
+
+	ret = scmi_to_linux_errno(out.status);
+	if (ret < 0)
+		return ret;
+
+	return out.voltage_level;
+}
+
+static int scmi_regulator_of_to_plat(struct udevice *dev)
+{
+	struct scmi_regulator_platdata *pdata = dev_get_plat(dev);
+	fdt_addr_t reg;
+
+	reg = dev_read_addr(dev);
+	if (reg == FDT_ADDR_T_NONE)
+		return -EINVAL;
+
+	pdata->domain_id = (u32)reg;
+
+	return 0;
+}
+
+static int scmi_regulator_probe(struct udevice *dev)
+{
+	struct scmi_regulator_platdata *pdata = dev_get_plat(dev);
+	struct scmi_voltd_attr_in in = { 0 };
+	struct scmi_voltd_attr_out out = { 0 };
+	struct scmi_msg scmi_msg = {
+		.protocol_id = SCMI_PROTOCOL_ID_VOLTAGE_DOMAIN,
+		.message_id = SCMI_VOLTAGE_DOMAIN_ATTRIBUTES,
+		.in_msg = (u8 *)&in,
+		.in_msg_sz = sizeof(in),
+		.out_msg = (u8 *)&out,
+		.out_msg_sz = sizeof(out),
+	};
+	int ret;
+
+	/* Check voltage domain is known from SCMI server */
+	in.domain_id = pdata->domain_id;
+
+	ret = devm_scmi_process_msg(dev->parent->parent, &scmi_msg);
+	if (ret) {
+		dev_err(dev, "Failed to query voltage domain %u: %d\n",
+			pdata->domain_id, ret);
+		return -ENXIO;
+	}
+
+	return 0;
+}
+
+static const struct dm_regulator_ops scmi_voltd_ops = {
+	.get_value = scmi_voltd_get_voltage_level,
+	.set_value = scmi_voltd_set_voltage_level,
+	.get_enable = scmi_voltd_get_enable,
+	.set_enable = scmi_voltd_set_enable,
+};
+
+U_BOOT_DRIVER(scmi_regulator) = {
+	.name = "scmi_regulator",
+	.id = UCLASS_REGULATOR,
+	.ops = &scmi_voltd_ops,
+	.probe = scmi_regulator_probe,
+	.of_to_plat = scmi_regulator_of_to_plat,
+	.plat_auto = sizeof(struct scmi_regulator_platdata),
+};
+
+static int scmi_regulator_bind(struct udevice *dev)
+{
+	struct driver *drv;
+	ofnode node;
+	int ret;
+
+	drv = DM_DRIVER_GET(scmi_regulator);
+
+	ofnode_for_each_subnode(node, dev_ofnode(dev)) {
+		ret = device_bind(dev, drv, ofnode_get_name(node),
+				  NULL, node, NULL);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
+U_BOOT_DRIVER(scmi_voltage_domain) = {
+	.name = "scmi_voltage_domain",
+	.id = UCLASS_NOP,
+	.bind = scmi_regulator_bind,
+};

drivers/power/regulator/Kconfig
@@ -353,3 +353,11 @@ config DM_REGULATOR_TPS65941
 	TPS65941 series of PMICs have 5 single phase BUCKs that can also
 	be configured in multi phase modes & 4 LDOs. The driver implements
 	get/set api for value and enable.
+
+config DM_REGULATOR_SCMI
+	bool "Enable driver for SCMI voltage domain regulators"
+	depends on DM_REGULATOR
+	select SCMI_AGENT
+        help
+          Enable this option if you want to support regulators exposed through
+	  the SCMI voltage domain protocol by a SCMI server.

drivers/power/regulator/Makefile
@@ -30,3 +30,4 @@ obj-$(CONFIG_DM_REGULATOR_TPS65910) += tps65910_regulator.o
 obj-$(CONFIG_DM_REGULATOR_TPS62360) += tps62360_regulator.o
 obj-$(CONFIG_$(SPL_)DM_REGULATOR_STPMIC1) += stpmic1.o
 obj-$(CONFIG_DM_REGULATOR_TPS65941) += tps65941_regulator.o
+obj-$(CONFIG_DM_REGULATOR_SCMI) += scmi_regulator.o

include/scmi_protocols.h
@@ -23,6 +23,7 @@ enum scmi_std_protocol {
 	SCMI_PROTOCOL_ID_CLOCK = 0x14,
 	SCMI_PROTOCOL_ID_SENSOR = 0x15,
 	SCMI_PROTOCOL_ID_RESET_DOMAIN = 0x16,
+	SCMI_PROTOCOL_ID_VOLTAGE_DOMAIN = 0x17,
 };
 
 enum scmi_status_code {
@@ -174,6 +175,118 @@ struct scmi_rd_reset_in {
  */
 struct scmi_rd_reset_out {
 	s32 status;
+};
+
+/*
+ * SCMI Voltage Domain Protocol
+ */
+
+enum scmi_voltage_domain_message_id {
+	SCMI_VOLTAGE_DOMAIN_ATTRIBUTES = 0x3,
+	SCMI_VOLTAGE_DOMAIN_CONFIG_SET = 0x5,
+	SCMI_VOLTAGE_DOMAIN_CONFIG_GET = 0x6,
+	SCMI_VOLTAGE_DOMAIN_LEVEL_SET = 0x7,
+	SCMI_VOLTAGE_DOMAIN_LEVEL_GET = 0x8,
+};
+
+#define SCMI_VOLTD_NAME_LEN		16
+
+#define SCMI_VOLTD_CONFIG_MASK		GENMASK(3, 0)
+#define SCMI_VOLTD_CONFIG_OFF		0
+#define SCMI_VOLTD_CONFIG_ON		0x7
+
+/**
+ * struct scmi_voltd_attr_in - Payload for VOLTAGE_DOMAIN_ATTRIBUTES message
+ * @domain_id:	SCMI voltage domain ID
+ */
+struct scmi_voltd_attr_in {
+	u32 domain_id;
+};
+
+/**
+ * struct scmi_voltd_attr_out - Payload for VOLTAGE_DOMAIN_ATTRIBUTES response
+ * @status:	SCMI command status
+ * @attributes:	Retrieved attributes of the voltage domain
+ * @name:	Voltage domain name
+ */
+struct scmi_voltd_attr_out {
+	s32 status;
+	u32 attributes;
+	char name[SCMI_VOLTD_NAME_LEN];
+};
+
+/**
+ * struct scmi_voltd_config_set_in - Message payload for VOLTAGE_CONFIG_SET cmd
+ * @domain_id:	SCMI voltage domain ID
+ * @config:	Configuration data of the voltage domain
+ */
+struct scmi_voltd_config_set_in {
+	u32 domain_id;
+	u32 config;
+};
+
+/**
+ * struct scmi_voltd_config_set_out - Response for VOLTAGE_CONFIG_SET command
+ * @status:	SCMI command status
+ */
+struct scmi_voltd_config_set_out {
+	s32 status;
+};
+
+/**
+ * struct scmi_voltd_config_get_in - Message payload for VOLTAGE_CONFIG_GET cmd
+ * @domain_id:	SCMI voltage domain ID
+ */
+struct scmi_voltd_config_get_in {
+	u32 domain_id;
+};
+
+/**
+ * struct scmi_voltd_config_get_out - Response for VOLTAGE_CONFIG_GET command
+ * @status:	SCMI command status
+ * @config:	Configuration data of the voltage domain
+ */
+struct scmi_voltd_config_get_out {
+	s32 status;
+	u32 config;
+};
+
+/**
+ * struct scmi_voltd_level_set_in - Message payload for VOLTAGE_LEVEL_SET cmd
+ * @domain_id:		SCMI voltage domain ID
+ * @flags:		Parameter flags for configuring target level
+ * @voltage_level:	Target voltage level in microvolts (uV)
+ */
+struct scmi_voltd_level_set_in {
+	u32 domain_id;
+	u32 flags;
+	s32 voltage_level;
+};
+
+/**
+ * struct scmi_voltd_level_set_out - Response for VOLTAGE_LEVEL_SET command
+ * @status:	SCMI	command status
+ */
+struct scmi_voltd_level_set_out {
+	s32 status;
+};
+
+/**
+ * struct scmi_voltd_level_get_in - Message payload for VOLTAGE_LEVEL_GET cmd
+ * @domain_id:		SCMI voltage domain ID
+ */
+struct scmi_voltd_level_get_in {
+	u32 domain_id;
+};
+
+/**
+ * struct scmi_voltd_level_get_out - Response for VOLTAGE_LEVEL_GET command
+ * @status:		SCMI command status
+ * @voltage_level:	Voltage level in microvolts (uV)
+ */
+struct scmi_voltd_level_get_out {
+	s32 status;
+	s32 voltage_level;
 };
 
 #endif /* _SCMI_PROTOCOLS_H */

commit 4a726d30cfb20fc5b230c0cee88bb921559b59e3

power: regulator: scmi: Move regulator subnode hack to scmi_regulator
The current code attempts to bind scmi_voltage_domain to regulator subnode
of the SCMI protocol node, so scmi_voltage_domain can then bind regulators
directly to subnodes of its node. This kind of behavior should not be in
core code, move it into scmi_voltage_domain driver code. Let the driver
descend into regulator node and bind regulators to its subnodes.

Fixes: 32470102409b ("firmware: scmi: voltage regulator")
Signed-off-by: Marek Vasut <marex@denx.de>
[Alice Guo: Fix scmi_regulator_bind]
Signed-off-by: Peng Fan <peng.fan@nxp.com>

drivers/firmware/scmi/scmi_agent-uclass.c
@@ -427,14 +427,8 @@ static int scmi_bind_protocols(struct udevice *dev)
 			break;
 		case SCMI_PROTOCOL_ID_VOLTAGE_DOMAIN:
 			if (IS_ENABLED(CONFIG_DM_REGULATOR_SCMI) &&
-			    scmi_protocol_is_supported(dev, protocol_id)) {
-				node = ofnode_find_subnode(node, "regulators");
-				if (!ofnode_valid(node)) {
-					dev_err(dev, "no regulators node\n");
-					return -ENXIO;
-				}
+			    scmi_protocol_is_supported(dev, protocol_id))
 				drv = DM_DRIVER_GET(scmi_voltage_domain);
-			}
 			break;
 		default:
 			break;

drivers/power/regulator/scmi_regulator.c
@@ -175,12 +175,19 @@ U_BOOT_DRIVER(scmi_regulator) = {
 static int scmi_regulator_bind(struct udevice *dev)
 {
 	struct driver *drv;
+	ofnode regul_node;
 	ofnode node;
 	int ret;
 
+	regul_node = ofnode_find_subnode(dev_ofnode(dev), "regulators");
+	if (!ofnode_valid(regul_node)) {
+		dev_err(dev, "no regulators node\n");
+		return -ENXIO;
+	}
+
 	drv = DM_DRIVER_GET(scmi_regulator);
 
-	ofnode_for_each_subnode(node, dev_ofnode(dev)) {
+	ofnode_for_each_subnode(node, regul_node) {
 		ret = device_bind(dev, drv, ofnode_get_name(node),
 				  NULL, node, NULL);
 		if (ret)