ThingLabsOSS/superbird-uboot

commit 4e3e1d24685abb931601d15906c2ff1b9712908a

bootstd: Allow scanning for global bootmeths separately
Typically we want to find and use global bootmeths first, since they have
the best idea of how the system should boot. We then use normal bootmeths
as a fallback.

Add the logic for this, putting global bootmeths at the end of the
ordering. We can then easily scan the global bootmeths first, then drop
them from the list for subsequent bootdev-centric scans.

This changes the ordering of global bootmeths, so update the
bootflow_system() accordingly.

Drop the comment from bootmeth_setup_iter_order() since this is an
exported function and it should be in the header file.

Signed-off-by: Simon Glass <sjg@chromium.org>

boot/bootmeth-uclass.c
@@ -85,18 +85,7 @@ int bootmeth_get_bootflow(struct udevice *dev, struct bootflow *bflow)
 	return ops->read_bootflow(dev, bflow);
 }
 
-/**
- * bootmeth_setup_iter_order() - Set up the ordering of bootmeths to scan
- *
- * This sets up the ordering information in @iter, based on the selected
- * ordering of the bootmethds in bootstd_priv->bootmeth_order. If there is no
- * ordering there, then all bootmethods are added
- *
- * @iter: Iterator to update with the order
- * Return: 0 if OK, -ENOENT if no bootdevs, -ENOMEM if out of memory, other -ve
- *	on other error
- */
-int bootmeth_setup_iter_order(struct bootflow_iter *iter)
+int bootmeth_setup_iter_order(struct bootflow_iter *iter, bool include_global)
 {
 	struct bootstd_priv *std;
 	struct udevice **order;
@@ -119,31 +108,77 @@ int bootmeth_setup_iter_order(struct bootflow_iter *iter)
 
 	/* If we have an ordering, copy it */
 	if (IS_ENABLED(CONFIG_BOOTSTD_FULL) && std->bootmeth_count) {
-		memcpy(order, std->bootmeth_order,
-		       count * sizeof(struct bootmeth *));
-	} else {
-		struct udevice *dev;
-		int i, upto;
+		int i;
 
 		/*
-		 * Get a list of bootmethods, in seq order (i.e. using aliases).
-		 * There may be gaps so try to count up high enough to find them
-		 * all.
+		 * We don't support skipping global bootmeths. Instead, the user
+		 * should omit them from the ordering
 		 */
-		for (i = 0, upto = 0; upto < count && i < 20 + count * 2; i++) {
-			ret = uclass_get_device_by_seq(UCLASS_BOOTMETH, i,
-						       &dev);
-			if (!ret)
-				order[upto++] = dev;
+		if (!include_global)
+			return log_msg_ret("glob", -EPERM);
+		memcpy(order, std->bootmeth_order,
+		       count * sizeof(struct bootmeth *));
+
+		if (IS_ENABLED(CONFIG_BOOTMETH_GLOBAL)) {
+			for (i = 0; i < count; i++) {
+				struct udevice *dev = order[i];
+				struct bootmeth_uc_plat *ucp;
+				bool is_global;
+
+				ucp = dev_get_uclass_plat(dev);
+				is_global = ucp->flags &
+					BOOTMETHF_GLOBAL;
+				if (is_global) {
+					iter->first_glob_method = i;
+					break;
+				}
+			}
+		}
+	} else {
+		struct udevice *dev;
+		int i, upto, pass;
+
+		/*
+		 * Do two passes, one to find the normal bootmeths and another
+		 * to find the global ones, if required, The global ones go at
+		 * the end.
+		 */
+		for (pass = 0, upto = 0; pass < 1 + include_global; pass++) {
+			if (pass)
+				iter->first_glob_method = upto;
+			/*
+			 * Get a list of bootmethods, in seq order (i.e. using
+			 * aliases). There may be gaps so try to count up high
+			 * enough to find them all.
+			 */
+			for (i = 0; upto < count && i < 20 + count * 2; i++) {
+				struct bootmeth_uc_plat *ucp;
+				bool is_global;
+
+				ret = uclass_get_device_by_seq(UCLASS_BOOTMETH,
+							       i, &dev);
+				if (ret)
+					continue;
+				ucp = dev_get_uclass_plat(dev);
+				is_global =
+					IS_ENABLED(CONFIG_BOOTMETH_GLOBAL) &&
+					(ucp->flags & BOOTMETHF_GLOBAL);
+				if (pass ? is_global : !is_global)
+					order[upto++] = dev;
+			}
 		}
 		count = upto;
 	}
 	if (!count)
 		return log_msg_ret("count2", -ENOENT);
 
+	if (IS_ENABLED(CONFIG_BOOTMETH_GLOBAL) && include_global &&
+	    iter->first_glob_method != -1 && iter->first_glob_method != count) {
+		iter->cur_method = iter->first_glob_method;
+		iter->doing_global = true;
+	}
 	iter->method_order = order;
 	iter->num_methods = count;
-	iter->cur_method = 0;
 
 	return 0;
 }

boot/bootflow.c
@@ -278,7 +278,7 @@ int bootflow_scan_bootdev(struct udevice *dev, struct bootflow_iter *iter,
 	if (ret)
 		return log_msg_ret("obdev", -ENODEV);
 
-	ret = bootmeth_setup_iter_order(iter);
+	ret = bootmeth_setup_iter_order(iter, !(flags & BOOTFLOWF_SKIP_GLOBAL));
 	if (ret)
 		return log_msg_ret("obmeth", -ENODEV);
 

include/bootmeth.h
@@ -209,10 +209,12 @@ int bootmeth_boot(struct udevice *dev, struct bootflow *bflow);
  * ordering there, then all bootmethods are added
  *
  * @iter: Iterator to update with the order
+ * @include_global: true to add the global bootmeths, in which case they appear
+ * first
  * Return: 0 if OK, -ENOENT if no bootdevs, -ENOMEM if out of memory, other -ve
  *	on other error
  */
-int bootmeth_setup_iter_order(struct bootflow_iter *iter);
+int bootmeth_setup_iter_order(struct bootflow_iter *iter, bool include_global);
 
 /**
  * bootmeth_set_order() - Set the bootmeth order

test/boot/bootflow.c
@@ -343,8 +343,9 @@ static int bootflow_system(struct unit_test_state *uts)
 	bootstd_clear_glob();
 	console_record_reset_enable();
 	ut_assertok(run_command("bootflow scan -l", 0));
-	ut_assert_skip_to_line("  1  bootmgr      ready   bootstd      0  <NULL>                    <NULL>");
-	ut_assert_nextline("No more bootdevs");
+	ut_assert_skip_to_line(
+		"  0  bootmgr      ready   (none)       0  <NULL>                    <NULL>");
+	ut_assert_skip_to_line("No more bootdevs");
 	ut_assert_skip_to_line("(2 bootflows, 2 valid)");
 	ut_assert_console_end();
 

commit cedf1df8be3d268870b1921eba7cc3a7affea732

bootstd: Probe bootmeth devices for bootmeths env var
Specifying efi_mgr in 'bootmeths' environment variable leads to NULL
pointer dereference when 'bootflow scan' is executed, with call trace
like this:

    priv->fake_dev // NULL pointer dereference
    .read_bootflow = efi_mgr_read_bootflow()
    bootmeth_get_bootflow()
    bootflow_check()
    bootflow_scan_first()
    do_bootflow_scan()
    'bootflow scan -l'

That happens because in case when 'bootmeths' env var is defined the
bootmeth_efi_mgr driver is not probed, and the memory for its private
data isn't allocated by .priv_auto. In case when 'bootmeths' env var is
not defined, the std->bootmeth_count is 0, and the execution flow in
bootmeth_setup_iter_order() takes "no ordering" path, which in turn runs
uclass_get_device_by_seq() -> ... -> device_probe(), so issue isn't
present there. But when 'bootmeths' is defined and contains efi_mgr, the
std->bootmeth_count > 0, so bootmeth_setup_iter_order() follows the "we
have an ordering" path, where devices are not probed. In other words:

    'bootmeths' defined           'bootmeths' not defined
    --------------------------------------------------------
    priv == NULL                    priv != NULL
         ^                                ^
         |                        device_alloc_priv()
     no probe                     device_of_to_plat()
         ^                        device_probe()
         |                        uclass_get_device_tail()
    dev = order[i]                uclass_get_device_by_seq()
         ^                                ^
         | have an ordering               | no ordering
         +----------------+---------------+
                          |
             bootmeth_setup_iter_order()
             bootflow_scan_first()
             do_bootflow_scan()

Add an explicit device_probe() call in "we have an ordering" case to fix
the issue.

Fixes: 4e3e1d24685a ("bootstd: Allow scanning for global bootmeths separately")

boot/bootmeth-uclass.c
@@ -12,6 +12,7 @@
 #include <bootmeth.h>
 #include <bootstd.h>
 #include <dm.h>
+#include <dm/device-internal.h>
 #include <env_internal.h>
 #include <fs.h>
 #include <malloc.h>
@@ -147,6 +148,12 @@ int bootmeth_setup_iter_order(struct bootflow_iter *iter, bool include_global)
 				struct udevice *dev = order[i];
 				struct bootmeth_uc_plat *ucp;
 				bool is_global;
+
+				ret = device_probe(dev);
+				if (ret) {
+					ret = log_msg_ret("probe", ret);
+					goto err_order;
+				}
 
 				ucp = dev_get_uclass_plat(dev);
 				is_global = ucp->flags &