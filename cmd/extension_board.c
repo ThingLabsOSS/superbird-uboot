ThingLabsOSS/superbird-uboot

commit 392f4c52c1708bfddc554d5f326f14b7e5f2c969

boot: Remove legacy extension board support
Remove the legacy extension board implementation now that all boards
have been converted to use the new UCLASS-based framework. This
eliminates lines of legacy code while preserving functionality
through the modern driver model approach.

Update the bootstd tests, due to the removal of extension hunter.

Signed-off-by: Kory Maincent (TI.com) <kory.maincent@bootlin.com>

cmd/extension_board.c
@@ -12,41 +12,23 @@
 static int do_extension_list(struct cmd_tbl *cmdtp, int flag,
 			     int argc, char *const argv[])
 {
-#if CONFIG_IS_ENABLED(SUPPORT_DM_EXTENSION_SCAN)
-	struct alist *dm_extension_list;
-#endif
+	struct alist *extension_list;
 	struct extension *extension;
 	int i = 0;
 
-#if CONFIG_IS_ENABLED(SUPPORT_DM_EXTENSION_SCAN)
-	dm_extension_list = dm_extension_get_list();
-
-	if (!alist_get_ptr(dm_extension_list, 0)) {
+	extension_list = extension_get_list();
+	if (!alist_get_ptr(extension_list, 0)) {
 		printf("No extension registered - Please run \"extension scan\"\n");
 		return CMD_RET_SUCCESS;
 	}
 
-	alist_for_each(extension, dm_extension_list) {
+	alist_for_each(extension, extension_list) {
 		printf("Extension %d: %s\n", i++, extension->name);
 		printf("\tManufacturer: \t\t%s\n", extension->owner);
 		printf("\tVersion: \t\t%s\n", extension->version);
 		printf("\tDevicetree overlay: \t%s\n", extension->overlay);
 		printf("\tOther information: \t%s\n", extension->other);
 	}
-#else
-	if (list_empty(&extension_list)) {
-		printf("No extension registered - Please run \"extension scan\"\n");
-		return CMD_RET_SUCCESS;
-	}
-
-	list_for_each_entry(extension, &extension_list, list) {
-		printf("Extension %d: %s\n", i++, extension->name);
-		printf("\tManufacturer: \t\t%s\n", extension->owner);
-		printf("\tVersion: \t\t%s\n", extension->version);
-		printf("\tDevicetree overlay: \t%s\n", extension->overlay);
-		printf("\tOther information: \t%s\n", extension->other);
-	}
-#endif
 	return CMD_RET_SUCCESS;
 }
 
@@ -55,69 +37,31 @@ static int do_extension_scan(struct cmd_tbl *cmdtp, int flag,
 {
 	int extension_num;
 
-#if CONFIG_IS_ENABLED(SUPPORT_DM_EXTENSION_SCAN)
-	extension_num = dm_extension_scan();
+	extension_num = extension_scan();
 	if (extension_num == -ENODEV)
 		extension_num = 0;
 	else if (extension_num < 0)
 		return CMD_RET_FAILURE;
 
 	printf("Found %d extension board(s).\n", extension_num);
-#else
-	extension_num = extension_scan(true);
-	if (extension_num < 0 && extension_num != -ENODEV)
-		return CMD_RET_FAILURE;
-#endif
-
 	return CMD_RET_SUCCESS;
 }
 
 static int do_extension_apply(struct cmd_tbl *cmdtp, int flag,
 			      int argc, char *const argv[])
 {
-#if !CONFIG_IS_ENABLED(SUPPORT_DM_EXTENSION_SCAN)
-	struct extension *extension = NULL;
-	struct list_head *entry;
-	int i = 0;
-#endif
-	int extension_id, ret;
+	int extension_id;
 
 	if (argc < 2)
 		return CMD_RET_USAGE;
 
 	if (strcmp(argv[1], "all") == 0) {
-		ret = CMD_RET_FAILURE;
-#if CONFIG_IS_ENABLED(SUPPORT_DM_EXTENSION_SCAN)
-		if (dm_extension_apply_all())
+		if (extension_apply_all())
 			return CMD_RET_FAILURE;
-#else
-		list_for_each_entry(extension, &extension_list, list) {
-			ret = extension_apply(extension);
-			if (ret != CMD_RET_SUCCESS)
-				break;
-		}
-#endif
 	} else {
 		extension_id = simple_strtol(argv[1], NULL, 10);
-#if CONFIG_IS_ENABLED(SUPPORT_DM_EXTENSION_SCAN)
-		if (dm_extension_apply(extension_id))
+		if (extension_apply(extension_id))
 			return CMD_RET_FAILURE;
-#else
-		list_for_each(entry, &extension_list) {
-			if (i == extension_id) {
-				extension = list_entry(entry, struct extension,  list);
-				break;
-			}
-			i++;
-		}
-
-		if (!extension) {
-			printf("Wrong extension number\n");
-			return CMD_RET_FAILURE;
-		}
-
-		ret = extension_apply(extension);
-#endif
 	}
 
 	return CMD_RET_SUCCESS;

MAINTAINERS
@@ -1188,7 +1188,6 @@ S:	Maintained
 F:	board/sunxi/chip.c
 F:	board/ti/common/cape_detect.c
 F:	boot/extension-uclass.c
-F:	boot/extension.c
 F:	cmd/extension_board.c
 F:	include/extension_board.h
 

arch/Kconfig
@@ -215,7 +215,7 @@ config SANDBOX
 	select SYSRESET_CMD_POWEROFF if CMD_POWEROFF
 	select SYS_CACHE_SHIFT_4
 	select IRQ
-	select SUPPORT_DM_EXTENSION_SCAN if CMDLINE
+	select SUPPORT_EXTENSION_SCAN if CMDLINE
 	select SUPPORT_ACPI
 	imply BITREVERSE
 	select BLOBLIST

arch/arm/mach-imx/imx8m/Kconfig
@@ -359,14 +359,14 @@ config TARGET_IMX8MM_CL_IOT_GATE
 	select IMX8MM
 	select SUPPORT_SPL
 	select IMX8M_LPDDR4
-	select SUPPORT_DM_EXTENSION_SCAN
+	select SUPPORT_EXTENSION_SCAN
 
 config TARGET_IMX8MM_CL_IOT_GATE_OPTEE
 	bool "CompuLab iot-gate-imx8 with optee support"
 	select IMX8MM
 	select SUPPORT_SPL
 	select IMX8M_LPDDR4
-	select SUPPORT_DM_EXTENSION_SCAN
+	select SUPPORT_EXTENSION_SCAN
 
 config TARGET_IMX8MP_RSB3720A1_4G
 	bool "Support i.MX8MP RSB3720A1 4G"

arch/arm/mach-omap2/am33xx/Kconfig
@@ -15,7 +15,7 @@ config TARGET_AM335X_EVM
 	select DM_GPIO
 	select DM_SERIAL
 	select TI_I2C_BOARD_DETECT
-	select SUPPORT_DM_EXTENSION_SCAN
+	select SUPPORT_EXTENSION_SCAN
 	imply CMD_DM
 	imply SPL_DM
 	imply SPL_DM_SEQ_ALIAS

arch/arm/mach-omap2/omap5/Kconfig
@@ -38,7 +38,7 @@ config TARGET_AM57XX_EVM
 	select CMD_DDR3
 	select DRA7XX
 	select TI_I2C_BOARD_DETECT
-	select SUPPORT_DM_EXTENSION_SCAN
+	select SUPPORT_EXTENSION_SCAN
 	imply DM_THERMAL
 	imply SCSI
 	imply SPL_THERMAL

arch/arm/mach-sunxi/Kconfig
@@ -1223,7 +1223,7 @@ config BLUETOOTH_DT_DEVICE_FIXUP
 
 config CHIP_DIP_SCAN
 	bool "Enable DIPs detection for CHIP board"
-	select SUPPORT_DM_EXTENSION_SCAN
+	select SUPPORT_EXTENSION_SCAN
 	select W1
 	select W1_GPIO
 	select W1_EEPROM

board/sandbox/sandbox.c
@@ -110,7 +110,7 @@ int ft_board_setup(void *fdt, struct bd_info *bd)
 	return fdt_add_mem_rsv(fdt, 0x00d02000, 0x4000);
 }
 
-#if CONFIG_IS_ENABLED(SUPPORT_DM_EXTENSION_SCAN) && \
+#if CONFIG_IS_ENABLED(SUPPORT_EXTENSION_SCAN) && \
 	!CONFIG_IS_ENABLED(XPL_BUILD)
 static int sandbox_extension_board_scan(struct udevice *dev,
 					struct alist *extension_list)

commit 02b10bde7c96e2458bf166980848f7412d141bd6

cmd/extension: avoid NULL pointer dereference
extension_get_list() will return NULL if there is no extension device.
Check for this situation.

Addresses-Coverity-ID: 638557 - Null pointer dereferences (NULL_RETURNS)
Fixes: 392f4c52c170 ("boot: Remove legacy extension board support")
Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
Acked-by: Kory Maincent <kory.maincent@bootlin.com>

cmd/extension_board.c
@@ -99,6 +99,10 @@ static int do_extension_list(struct cmd_tbl *cmdtp, int flag,
 	int i = 0;
 
 	extension_list = extension_get_list();
+	if (!extension_list) {
+		printf("No extension device\n");
+		return CMD_RET_FAILURE;
+	}
 	if (!alist_get_ptr(extension_list, 0)) {
 		printf("No extension registered - Please run \"extension scan\"\n");
 		return CMD_RET_SUCCESS;