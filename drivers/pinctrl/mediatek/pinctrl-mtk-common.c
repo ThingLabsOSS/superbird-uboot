ThingLabsOSS/superbird-uboot

commit ebbdde5db6749e4c19b993c7398a636126804e27

pinctrl: mediatek: support mediatek,pctl-regmap property
Add support for the mediatek,pctl-regmap devicetree property to the
common MediaTek pinctrl driver.

In upstream devicetrees from Linux, the pinctrl nodes may be on the
interrupt controller register address space rather than the pinctrl
register address space. In this case, there is a syscon node linking to
the actual pinctrl registers. This uses a common property name of
mediatek,pctl-regmap for the phandle to the syscon node.

The logic here is that if this property is present, we look up the
syscon node and use it's address as the base address of the pinctrl
registers and ignore the pinctrl node's own reg property. (Support
for interrupts could be added later if needed.)

There is also at least one SoC in Linux that has two syscon phandles
in this property. This implementation support parsing this, but doesn't
do anything with the second syscon yet (the 2nd syscon is for interrupts
which we are saving for later).

Signed-off-by: David Lechner <dlechner@baylibre.com>

drivers/pinctrl/mediatek/pinctrl-mtk-common.c
@@ -809,8 +809,46 @@ int mtk_pinctrl_common_probe(struct udevice *dev,
 	fdt_addr_t addr;
 	u32 base_calc = soc->base_calc;
 	u32 nbase_names = soc->nbase_names;
+	int num_regmaps;
 
 	priv->soc = soc;
+
+	/*
+	 * Some controllers have 1 or 2 syscon nodes where the actual pinctl
+	 * registers reside. In this case, dev is an interrupt controller which
+	 * isn't supported at this time. The optional 2nd syscon node is also
+	 * for the interrupt controller, so we only use the 1st one currently.
+	 */
+	num_regmaps = dev_count_phandle_with_args(dev, "mediatek,pctl-regmap", NULL, 0);
+	if (num_regmaps > ARRAY_SIZE(priv->base))
+		return -EINVAL;
+
+	if (num_regmaps > 0) {
+		for (i = 0; i < num_regmaps; i++) {
+			struct ofnode_phandle_args args;
+			struct udevice *syscon_dev;
+			int ret;
+
+			ret = dev_read_phandle_with_args(dev, "mediatek,pctl-regmap",
+							 NULL, 0, i, &args);
+			if (ret)
+				return ret;
+
+			ret = uclass_get_device_by_ofnode(UCLASS_SYSCON,
+							  args.node,
+							  &syscon_dev);
+			if (ret)
+				return ret;
+
+			addr = dev_read_addr_index(syscon_dev, 0);
+			if (addr == FDT_ADDR_T_NONE)
+				return -EINVAL;
+
+			priv->base[i] = (void __iomem *)addr;
+		}
+
+		return 0;
+	}
 
 	if (!base_calc)
 		nbase_names = 1;

commit 75d81979ee1eacf6ca4db729f68a03ac4e4e4e65

pinctrl: mediatek: fix failing to get syscon
Replace uclass_get_device_by_ofnode() with syscon_regmap_lookup_by_phandle()
to get the "mediatek,pctl-regmap" syscon device.

Depending on probe order, uclass_get_device_by_ofnode() may fail, but
syscon_regmap_lookup_by_phandle() has logic in it to handle that case
correctly.

The previous implementation could read more than one syscon if the
"mediatek,pctl-regmap" property had more than one phandle, but the one
board with a devicetree that does that is not supported in U-Boot yet,
so we can save that for later (it may never be needed).

Fixes: ebbdde5db674 ("pinctrl: mediatek: support mediatek,pctl-regmap property")
Signed-off-by: David Lechner <dlechner@baylibre.com>

drivers/pinctrl/mediatek/Kconfig
@@ -2,6 +2,8 @@ if ARCH_MEDIATEK
 
 config PINCTRL_MTK
 	depends on PINCTRL_GENERIC
+	select REGMAP
+	select SYSCON
 	bool
 
 config PINCTRL_MT7622

drivers/pinctrl/mediatek/pinctrl-mtk-common.c
@@ -11,6 +11,10 @@
 #include <asm/io.h>
 #include <asm-generic/gpio.h>
 #include <linux/bitops.h>
+#include <linux/err.h>
+#include <log.h>
+#include <regmap.h>
+#include <syscon.h>
 
 #include "pinctrl-mtk-common.h"
 
@@ -822,32 +826,17 @@ int mtk_pinctrl_common_probe(struct udevice *dev,
 	 * for the interrupt controller, so we only use the 1st one currently.
 	 */
 	num_regmaps = dev_count_phandle_with_args(dev, "mediatek,pctl-regmap", NULL, 0);
-	if (num_regmaps > ARRAY_SIZE(priv->base))
-		return -EINVAL;
 
 	if (num_regmaps > 0) {
-		for (i = 0; i < num_regmaps; i++) {
-			struct ofnode_phandle_args args;
-			struct udevice *syscon_dev;
-			int ret;
+		struct regmap *regmap;
 
-			ret = dev_read_phandle_with_args(dev, "mediatek,pctl-regmap",
-							 NULL, 0, i, &args);
-			if (ret)
-				return ret;
+		regmap = syscon_regmap_lookup_by_phandle(dev, "mediatek,pctl-regmap");
+		if (IS_ERR(regmap))
+			return log_msg_ret("regmap: ", PTR_ERR(regmap));
 
-			ret = uclass_get_device_by_ofnode(UCLASS_SYSCON,
-							  args.node,
-							  &syscon_dev);
-			if (ret)
-				return ret;
-
-			addr = dev_read_addr_index(syscon_dev, 0);
-			if (addr == FDT_ADDR_T_NONE)
-				return -EINVAL;
-
-			priv->base[i] = (void __iomem *)addr;
-		}
+		priv->base[0] = regmap_get_range(regmap, 0);
+		if (!priv->base[0])
+			return log_msg_ret("range: ", -EINVAL);
 
 		return 0;
 	}