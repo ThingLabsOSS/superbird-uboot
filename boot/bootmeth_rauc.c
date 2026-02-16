ThingLabsOSS/superbird-uboot

commit 8d9b699dc64f215bbcb14e7edd65000cefef6042

bootstd: rauc: Don't check root part filesystem
Only check if the root partition exists when scanning for the slots
partitions and not if the filesystem can be accessed. It is not needed
to access the filesystem of the root partition as it might not be
supported by u-boot or be encrypted.

Signed-off-by: Leonard Anderweit <l.anderweit@phytec.de>
Tested-by: Martin Schwan <m.schwan@phytec.de>

boot/bootmeth_rauc.c
@@ -17,6 +17,7 @@
 #include <fs.h>
 #include <malloc.h>
 #include <mapmem.h>
+#include <part.h>
 #include <string.h>
 #include <linux/stringify.h>
 #include <asm/cache.h>
@@ -121,10 +122,9 @@ static int distro_rauc_scan_parts(struct bootflow *bflow)
 			if (ret)
 				return log_msg_ret("part", ret);
 			fs_close();
-			ret = fs_set_blk_dev_with_part(desc, slot->root_part);
+			ret = part_get_info(desc, slot->root_part, NULL);
 			if (ret)
 				return log_msg_ret("part", ret);
-			fs_close();
 		}
 	}
 	str_free_list(boot_order_list);

commit de6cf1980caafc35af6b52c52603a558c7077a47

bootstd: rauc: Fix null pointer access while checking root part
Fix a segmentation fault caused by a null pointer access during root
partition checking. The function part_get_info() was falsely given null
for the disk_partition struct, which later resulted in accessing a null
pointer and thus undefined behavior.

Fixes: 8d9b699dc64f ("bootstd: rauc: Don't check root part filesystem")
Signed-off-by: Martin Schwan <m.schwan@phytec.de>

boot/bootmeth_rauc.c
@@ -97,6 +97,7 @@ static int distro_rauc_scan_parts(struct bootflow *bflow)
 {
 	struct blk_desc *desc;
 	struct distro_rauc_priv *priv;
+	struct disk_partition fs_info;
 	char *boot_order;
 	const char **boot_order_list;
 	bool slot_found = false;
@@ -123,7 +124,7 @@ static int distro_rauc_scan_parts(struct bootflow *bflow)
 			if (ret)
 				continue;
 			fs_close();
-			ret = part_get_info(desc, slot->root_part, NULL);
+			ret = part_get_info(desc, slot->root_part, &fs_info);
 			if (ret)
 				continue;
 			slot_found = true;