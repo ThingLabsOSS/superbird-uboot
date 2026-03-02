ThingLabsOSS/superbird-uboot

commit e54391089b266931fee6d6c8669e5f9180dd0f45

ufs: Call ufs_scsi_bind() from uclass .post_bind
Instead of duplicating the ufs_scsi_bind() call in every driver,
call it from UFS uclass .post_bind callback for every driver in
one place. While doing so, inline ufs_scsi_bind() directly into
ufs_post_bind() as trivial device_bind_driver() call.

Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>
Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>
Link: https://patch.msgid.link/20251028142335.18125-5-marek.vasut+renesas@mailbox.org
[narmstrong: also updated the rockchip and mediatek drivers]
Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

drivers/ufs/cdns-platform.c
@@ -101,13 +101,6 @@ static int cdns_ufs_pltfm_probe(struct udevice *dev)
 	return err;
 }
 
-static int cdns_ufs_pltfm_bind(struct udevice *dev)
-{
-	struct udevice *scsi_dev;
-
-	return ufs_scsi_bind(dev, &scsi_dev);
-}
-
 static const struct udevice_id cdns_ufs_pltfm_ids[] = {
 	{
 		.compatible = "cdns,ufshc-m31-16nm",
@@ -120,5 +113,4 @@ U_BOOT_DRIVER(cdns_ufs_pltfm) = {
 	.id		= UCLASS_UFS,
 	.of_match	= cdns_ufs_pltfm_ids,
 	.probe		= cdns_ufs_pltfm_probe,
-	.bind		= cdns_ufs_pltfm_bind,
 };

drivers/ufs/ufs-amd-versal2.c
@@ -552,13 +552,6 @@ static int ufs_versal2_probe(struct udevice *dev)
 	return ret;
 }
 
-static int ufs_versal2_bind(struct udevice *dev)
-{
-	struct udevice *scsi_dev;
-
-	return ufs_scsi_bind(dev, &scsi_dev);
-}
-
 static const struct udevice_id ufs_versal2_ids[] = {
 	{
 		.compatible = "amd,versal2-ufs",
@@ -571,5 +564,4 @@ U_BOOT_DRIVER(ufs_versal2_pltfm) = {
 	.id		= UCLASS_UFS,
 	.of_match	= ufs_versal2_ids,
 	.probe		= ufs_versal2_probe,
-	.bind		= ufs_versal2_bind,
 };

drivers/ufs/ufs-pci.c
@@ -11,13 +11,6 @@
 #include <dm/device_compat.h>
 #include "ufs.h"
 
-static int ufs_pci_bind(struct udevice *dev)
-{
-	struct udevice *scsi_dev;
-
-	return ufs_scsi_bind(dev, &scsi_dev);
-}
-
 static int ufs_pci_probe(struct udevice *dev)
 {
 	int err;
@@ -32,7 +25,6 @@ static int ufs_pci_probe(struct udevice *dev)
 U_BOOT_DRIVER(ufs_pci) = {
 	.name	= "ufs_pci",
 	.id	= UCLASS_UFS,
-	.bind	= ufs_pci_bind,
 	.probe	= ufs_pci_probe,
 };
 

drivers/ufs/ufs-qcom.c
@@ -648,13 +648,6 @@ static int ufs_qcom_probe(struct udevice *dev)
 	return 0;
 }
 
-static int ufs_qcom_bind(struct udevice *dev)
-{
-	struct udevice *scsi_dev;
-
-	return ufs_scsi_bind(dev, &scsi_dev);
-}
-
 static const struct udevice_id ufs_qcom_ids[] = {
 	{ .compatible = "qcom,ufshc" },
 	{},
@@ -665,6 +658,5 @@ U_BOOT_DRIVER(qcom_ufshcd) = {
 	.id		= UCLASS_UFS,
 	.of_match	= ufs_qcom_ids,
 	.probe		= ufs_qcom_probe,
-	.bind		= ufs_qcom_bind,
 	.priv_auto	= sizeof(struct ufs_qcom_priv),
 };

drivers/ufs/ufs-renesas.c
@@ -351,13 +351,6 @@ static struct ufs_hba_ops ufs_renesas_vops = {
 	.hce_enable_notify = ufs_renesas_hce_enable_notify,
 };
 
-static int ufs_renesas_pltfm_bind(struct udevice *dev)
-{
-	struct udevice *scsi_dev;
-
-	return ufs_scsi_bind(dev, &scsi_dev);
-}
-
 static int ufs_renesas_pltfm_probe(struct udevice *dev)
 {
 	struct ufs_renesas_priv *priv = dev_get_priv(dev);
@@ -405,7 +398,6 @@ U_BOOT_DRIVER(ufs_renesas) = {
 	.name		= "ufs-renesas",
 	.id		= UCLASS_UFS,
 	.of_match	= ufs_renesas_pltfm_ids,
-	.bind		= ufs_renesas_pltfm_bind,
 	.probe		= ufs_renesas_pltfm_probe,
 	.remove		= ufs_renesas_pltfm_remove,
 	.priv_auto	= sizeof(struct ufs_renesas_priv),

drivers/ufs/ufs-rockchip.c
@@ -194,18 +194,10 @@ static int ufs_rockchip_probe(struct udevice *dev)
 	return err;
 }
 
-static int ufs_rockchip_bind(struct udevice *dev)
-{
-	struct udevice *scsi_dev;
-
-	return ufs_scsi_bind(dev, &scsi_dev);
-}
-
 U_BOOT_DRIVER(rockchip_ufs) = {
 	.name		= "ufshcd-rockchip",
 	.id		= UCLASS_UFS,
 	.of_match	= ufs_rockchip_of_match,
 	.probe		= ufs_rockchip_probe,
-	.bind		= ufs_rockchip_bind,
 	.priv_auto	= sizeof(struct ufs_rockchip_host),
 };

drivers/ufs/ufs-mediatek.c
@@ -381,13 +381,6 @@ static int ufs_mtk_probe(struct udevice *dev)
 	return 0;
 }
 
-static int ufs_mtk_bind(struct udevice *dev)
-{
-	struct udevice *scsi_dev;
-
-	return ufs_scsi_bind(dev, &scsi_dev);
-}
-
 static const struct udevice_id ufs_mtk_ids[] = {
 	{ .compatible = "mediatek,mt6878-ufshci" },
 	{},
@@ -398,6 +391,5 @@ U_BOOT_DRIVER(mediatek_ufshci) = {
 	.id		= UCLASS_UFS,
 	.of_match	= ufs_mtk_ids,
 	.probe		= ufs_mtk_probe,
-	.bind		= ufs_mtk_bind,
 	.priv_auto	= sizeof(struct ufs_mtk_host),
 };

commit 5d1ba8380baaef363debc7467cb82245076a9534

drivers: ufs: remove unused ufs_post_bind() declaration
Commit e54391089b26 ("ufs: Call ufs_scsi_bind() from uclass .post_bind")
inlined ufs_scsi_bind() into ufs_post_bind() as trivial
device_bind_driver() call.

ufs_scsi_bind() is no longer referenced anywhere in the codebase, so
drop its declaration from include/ufs.h.

Drivers used to include <ufs.h> to include prototype of ufs_scsi_bind()
function, so we can now safely remove such includes.

Fixes: e54391089b26 ("ufs: Call ufs_scsi_bind() from uclass .post_bind")
Signed-off-by: Julien Stephan <jstephan@baylibre.com>
Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>
Link: https://patch.msgid.link/20260203-cleanup-ufs-header-v1-1-4c10424485f0@baylibre.com
Signed-off-by: Neil Armstrong <neil.armstrong@linaro.org>

drivers/ufs/cdns-platform.c
@@ -7,7 +7,6 @@
 
 #include <clk.h>
 #include <dm.h>
-#include <ufs.h>
 #include <asm/io.h>
 #include <dm/device_compat.h>
 #include <linux/bitops.h>

drivers/ufs/ufs-amd-versal2.c
@@ -5,7 +5,6 @@
 
 #include <clk.h>
 #include <dm.h>
-#include <ufs.h>
 #include <asm/io.h>
 #include <dm/device_compat.h>
 #include <zynqmp_firmware.h>

drivers/ufs/ufs-pci.c
@@ -7,7 +7,6 @@
 #include <dm.h>
 #include <errno.h>
 #include <pci.h>
-#include <ufs.h>
 #include <dm/device_compat.h>
 #include "ufs.h"
 

drivers/ufs/ufs-qcom.c
@@ -14,7 +14,6 @@
 #include <dm.h>
 #include <dm/device_compat.h>
 #include <generic-phy.h>
-#include <ufs.h>
 #include <asm/gpio.h>
 #include <interconnect.h>
 

drivers/ufs/ufs-renesas-rcar-gen5.c
@@ -7,7 +7,6 @@
 
 #include <clk.h>
 #include <dm.h>
-#include <ufs.h>
 #include <asm/io.h>
 #include <dm/device_compat.h>
 #include <linux/iopoll.h>

drivers/ufs/ufs-renesas.c
@@ -7,7 +7,6 @@
 
 #include <clk.h>
 #include <dm.h>
-#include <ufs.h>
 #include <asm/io.h>
 #include <dm/device_compat.h>
 #include <linux/bitops.h>

drivers/ufs/ufs-rockchip.c
@@ -13,7 +13,6 @@
 #include <linux/err.h>
 #include <linux/ioport.h>
 #include <reset.h>
-#include <ufs.h>
 
 #include "ufs.h"
 #include "unipro.h"

drivers/ufs/ufshcd-dwc.c
@@ -7,7 +7,6 @@
  */
 #include <clk.h>
 #include <dm.h>
-#include <ufs.h>
 #include <asm/io.h>
 #include <dm/device_compat.h>
 #include <linux/bitops.h>

include/ufs.h
@@ -20,13 +20,4 @@ int ufs_probe(void);
  */
 int ufs_probe_dev(int index);
 
-/*
- * ufs_scsi_bind() - Create a new scsi device as a child of the UFS device and
- *		     bind it to the ufs_scsi driver
- * @ufs_dev: UFS device
- * @scsi_devp: Pointer to scsi device
- *
- * Return: 0 if Ok, -ve on error
- */
-int ufs_scsi_bind(struct udevice *ufs_dev, struct udevice **scsi_devp);
 #endif